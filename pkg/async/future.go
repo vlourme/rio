b4be4a5a7745400e4b255089fe3ac96ed8101c29
wangminxiang <ryougi.nevermore@hotmail.com>
2024-10-27 22:31:08 +0800

    refactor

--- pkg/async/future.go
+++ pkg/async/future.go
@@ -6,35 +6,81 @@ var (
 	ErrFutureClosed = errors.New("rio: future closed")
 )
 
+type Promise[R any] interface {
+	Complete(result R, err error)
+	Succeed(result R)
+	Fail(cause error)
+	Future() (future Future[R])
+}
+
 type Future[R any] interface {
 	OnComplete(handler ResultHandler[R])
 }
 
 func SucceedFuture[R any](result R) Future[R] {
 	ch := make(chan Result[R], 1)
-	ch <- newAsyncResult[R](result, nil)
+	ch <- newSucceedAsyncResult[R](result)
 	return newFuture[R](ch)
 }
 
 func FailedFuture[R any](cause error) Future[R] {
 	ch := make(chan Result[R], 1)
-	ch <- newAsyncResult[R](nil, cause)
+	ch <- newFailedAsyncResult[R](cause)
 	return newFuture[R](ch)
 }
 
-func newFuture[R any](ch <-chan Result[R]) Future[R] {
+func New[R any]() Promise[R] {
+	return &futureImpl[R]{
+		ch: make(chan Result[R], 1),
+	}
+}
+
+func newFuture[R any](ch chan Result[R]) Future[R] {
 	return &futureImpl[R]{ch: ch}
 }
 
 type futureImpl[R any] struct {
-	ch <-chan Result[R]
+	ch      chan Result[R]
+	handler ResultHandler[R]
 }
 
 func (f *futureImpl[R]) OnComplete(handler ResultHandler[R]) {
+	f.setHandler(handler)
+}
+
+func (f *futureImpl[R]) Complete(result R, err error) {
+	if err == nil {
+		f.Succeed(result)
+	} else {
+		f.Fail(err)
+	}
+}
+
+func (f *futureImpl[R]) Succeed(result R) {
+	f.ch <- newSucceedAsyncResult[R](result)
+	close(f.ch)
+}
+
+func (f *futureImpl[R]) Fail(cause error) {
+	f.ch <- newFailedAsyncResult[R](cause)
+	close(f.ch)
+}
+
+func (f *futureImpl[R]) Future() (future Future[R]) {
+	future = f
+	return
+}
+
+func (f *futureImpl[R]) setHandler(handler ResultHandler[R]) {
+	f.handler = handler
+	defaultRunnable.Run(f.await)
+}
+
+func (f *futureImpl[R]) await() {
 	ar, ok := <-f.ch
 	if !ok {
-		handler(nil, ErrFutureClosed)
+		f.handler(*new(R), ErrFutureClosed)
 		return
 	}
-	handler(ar.Result(), ar.Cause())
+	f.handler(ar.Result(), ar.Cause())
 }
