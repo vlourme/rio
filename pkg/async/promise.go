fb16004978404007001261dc680697d32fa9c62d
wangminxiang <ryougi.nevermore@hotmail.com>
2024-10-28 19:46:51 +0800

    refactor

---
+++ pkg/async/promise.go
@@ -0,0 +1,57 @@
+package async
+
+import (
+	"context"
+	"runtime"
+	"time"
+)
+
+type Promise[R any] interface {
+	Complete(result R, err error)
+	Succeed(result R)
+	Fail(cause error)
+	Future() (future Future[R])
+}
+
+func TryGetPromise[T any](ctx context.Context) (promise Promise[T], ok bool) {
+	exec := From(ctx)
+	// todo avalibale or exec
+	ch, has := exec.TryGetExecutorChan()
+	if !has {
+		return
+	}
+	promise = newPromise[T](ctx, ch)
+	ok = true
+	return
+}
+
+func GetPromise[T any](ctx context.Context) (promise Promise[T], err error) {
+	times := 10
+	ok := false
+	for {
+		// todo avalibale or exec
+		promise, ok = TryGetPromise[T](ctx)
+		if ok {
+			break
+		}
+		deadline, hasDeadline := ctx.Deadline()
+		if hasDeadline && deadline.Before(time.Now()) {
+			err = ErrGetPromiseFailedByTimeout
+			break
+		}
+		times--
+		if times < 0 {
+			times = 10
+			runtime.Gosched()
+		}
+	}
+	return
+}
+
+func newPromise[R any](ctx context.Context, exec ExecutorChan) Promise[R] {
+	return &futureImpl[R]{
+		ctx:  ctx,
+		ch:   make(chan Result[R], 1),
+		exec: exec,
+	}
+}
