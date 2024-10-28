529ca809502dc7b0ecb6a494d4c1440d413458a1
wangminxiang <ryougi.nevermore@hotmail.com>
2024-10-28 19:47:04 +0800

    mod

--- pkg/async/future_test.go
+++ pkg/async/future_test.go
@@ -1,33 +1,13 @@
 package async_test
 
 import (
-	"errors"
-	"github.com/brickingsoft/rio/pkg/async"
-	"net"
-	"sync"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	p := async.New[int]()
-	p.Future().OnComplete(func(result int, err error) {
-		t.Log("result:", result, "err:", err)
-		wg.Done()
-	})
-	p.Succeed(1)
-	wg.Wait()
+
 }
 
 func TestFailedFuture(t *testing.T) {
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	p := async.New[net.Listener]()
-	p.Future().OnComplete(func(result net.Listener, err error) {
-		t.Log("result:", result, "err:", err, result == nil)
-		wg.Done()
-	})
-	p.Fail(errors.New("fail"))
-	wg.Wait()
+
 }
