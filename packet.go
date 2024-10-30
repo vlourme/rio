1a75e40c1a0c1237eee9ee29dff1d5667ae4efa6
wangminxiang <ryougi.nevermore@hotmail.com>
2024-10-30 19:47:29 +0800

    add packet

--- packet.go
+++ packet.go
@@ -2,21 +2,32 @@ package rio
 
 import (
 	"context"
+	"github.com/brickingsoft/rio/pkg/async"
+	"github.com/brickingsoft/rio/pkg/bytebufferpool"
 	"net"
 	"time"
 )
 
 // ListenPacket
-func ListenPacket(ctx context.Context, network string, addr string, options ...Option) (conn PacketConn, err error) {
+// "udp", "udp4", "udp6", "unixgram"
+func ListenPacket(ctx context.Context, network string, addr string, options ...Option) (conn PacketConnection, err error) {
 
 	return
 }
 
-type PacketConn interface {
-	ReadFrom(p []byte) (n int, addr net.Addr, err error)
-	WriteTo(p []byte, addr net.Addr) (n int, err error)
-	Close() error
+type PacketInbound interface {
+	Buffer() (buf bytebufferpool.Buffer)
+	Bytes() (n int)
+	Addr() (addr net.Addr)
+}
+
+type PacketConnection interface {
+	LocalAddr() (addr net.Addr)
 	SetDeadline(t time.Time) error
 	SetReadDeadline(t time.Time) error
 	SetWriteDeadline(t time.Time) error
+	SetReadBufferSize(size int)
+	ReadFrom() (future async.Future[PacketInbound])
+	WriteTo(p []byte, addr net.Addr) (future async.Future[Outbound])
+	Close() error
 }
