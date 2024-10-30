a5350a6c5ac49ca7b9b4bdec8b46ecdb090408ef
wangminxiang <ryougi.nevermore@hotmail.com>
2024-10-30 20:57:08 +0800

    mod win sock

--- pkg/sockets/tcpsock_windows.go
+++ pkg/sockets/tcpsock_windows.go
@@ -7,14 +7,7 @@ import (
 	"net"
 )
 
-type TCPOperationResultHandler func(mode OperationMode, conn *TCPConnection, p []byte, n int, cause error)
-
-func Listen() (ln *TCPListener, err error) {
-
-	return
-}
-
-type TCPListener struct {
+type tcpListener struct {
 	iocp   windows.Handle
 	fd     windows.Handle
 	addr   net.Addr
@@ -23,25 +16,25 @@ type TCPListener struct {
 	net    string
 }
 
-func (ln *TCPListener) Addr() (addr net.Addr) {
+func (ln *tcpListener) Addr() (addr net.Addr) {
 	addr = ln.addr
 	return
 }
 
-func (ln *TCPListener) Accept(handler TCPOperationResultHandler) (err error) {
+func (ln *tcpListener) Accept() (err error) {
 
 	return
 }
 
-func (ln *TCPListener) Close() (err error) {
+func (ln *tcpListener) Close() (err error) {
 	return
 }
 
-func (ln *TCPListener) loop() {
+func (ln *tcpListener) polling() {
 
 }
 
-type TCPConnection struct {
+type tcpConnection struct {
 	iocp       windows.Handle
 	ln         windows.Handle
 	fd         windows.Handle
