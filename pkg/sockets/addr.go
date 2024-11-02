6a69687d62c39d96387ab90b5ea34c5c50387765
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-02 19:34:19 +0800

    mod GetAddrAndFamily

--- pkg/sockets/addr.go
+++ pkg/sockets/addr.go
@@ -1,43 +1,172 @@
 package sockets
 
 import (
+	"errors"
+	"fmt"
 	"net"
+	"net/netip"
+	"strconv"
+	"strings"
 	"syscall"
-	_ "unsafe"
 )
 
-func getAddrFamily(network string, addr net.Addr) (family int, ipv6only bool) {
-	switch network {
-	case "tcp", "udp":
-		switch add := addr.(type) {
-		case *net.TCPAddr:
-			ipv6only = add.AddrPort().Addr().Is6()
-			if ipv6only {
-				family = syscall.AF_INET6
-			}
-			break
-		case *net.UDPAddr:
-			ipv6only = add.AddrPort().Addr().Is6()
-			if ipv6only {
-				family = syscall.AF_INET6
-			}
-			break
-		default:
-			family = syscall.AF_UNSPEC
-			break
+func GetAddrAndFamily(network string, addr string) (v net.Addr, family int, ipv6only bool, err error) {
+	addr = strings.TrimSpace(addr)
+	if addr == "" {
+		err = errors.New("invalid addr")
+		return
+	}
+	// unix
+	if network == "unix" || network == "unixgram" || network == "unixpacket" {
+		v, err = net.ResolveUnixAddr(network, addr)
+		if err == nil {
+			return
 		}
-	case "tcp4", "udp4":
-		family = syscall.AF_INET
-	case "tcp6", "udp6":
-		family = syscall.AF_INET6
-		ipv6only = true
-		break
-	case "unix", "unixgram", "unixpacket":
 		family = syscall.AF_UNIX
+		return
+	}
+	// parse addr
+	ap, parseAddrErr := ParseAddrPort(addr)
+	if parseAddrErr != nil {
+		err = parseAddrErr
+		return
+	}
+	if !ap.IsValid() {
+		err = errors.New("invalid addr")
+		return
+	}
+	ip := ap.Addr().AsSlice()
+	ipLen := len(ip)
+	if ipLen == net.IPv6len {
+		ipv6only = true
+	}
+	port := int(ap.Port())
+	switch network {
+	case "tcp":
+		if ipv6only {
+			v = &net.TCPAddr{
+				IP:   ip,
+				Port: port,
+				Zone: ap.Addr().Zone(),
+			}
+			family = syscall.AF_INET6
+		} else {
+			if ipLen == 0 {
+				ip = net.IPv4zero
+			}
+			v = &net.TCPAddr{
+				IP:   ip,
+				Port: port,
+				Zone: "",
+			}
+			family = syscall.AF_INET
+		}
 		break
-	default:
-		family = syscall.AF_UNSPEC
+	case "tcp4":
+		if ipv6only {
+			err = errors.New("tcp4 is not supported on IPv4")
+			return
+		}
+		if ipLen == 0 {
+			ip = net.IPv4zero
+		}
+		v = &net.TCPAddr{
+			IP:   ip,
+			Port: port,
+			Zone: "",
+		}
+		family = syscall.AF_INET
+		break
+	case "tcp6":
+		if ipv6only {
+			err = errors.New("tcp6 is not supported on IPv6")
+			return
+		}
+		v = &net.TCPAddr{
+			IP:   ip,
+			Port: port,
+			Zone: ap.Addr().Zone(),
+		}
+		family = syscall.AF_INET6
+		break
+	case "udp":
+		if ipv6only {
+			v = &net.UDPAddr{
+				IP:   ip,
+				Port: port,
+				Zone: ap.Addr().Zone(),
+			}
+			family = syscall.AF_INET6
+		} else {
+			if ipLen == 0 {
+				ip = net.IPv4zero
+			}
+			v = &net.UDPAddr{
+				IP:   ip,
+				Port: port,
+				Zone: "",
+			}
+			family = syscall.AF_INET
+		}
+		break
+	case "udp4":
+		if ipv6only {
+			err = errors.New("udp4 is not supported on IPv4")
+			return
+		}
+		if ipLen == 0 {
+			ip = net.IPv4zero
+		}
+		v = &net.UDPAddr{
+			IP:   ip,
+			Port: port,
+			Zone: "",
+		}
+		family = syscall.AF_INET
+		break
+	case "udp6":
+		if ipv6only {
+			err = errors.New("udp6 is not supported on IPv6")
+			return
+		}
+		v = &net.UDPAddr{
+			IP:   ip,
+			Port: port,
+			Zone: ap.Addr().Zone(),
+		}
+		family = syscall.AF_INET6
 		break
 	}
+	return
+}
+
+func ParseAddrPort(addr string) (addrPort netip.AddrPort, err error) {
+	i := strings.LastIndexByte(addr, ':')
+	if i == -1 {
+		err = errors.New("not an ip:port")
+		return
+	}
+	ip, port := addr[:i], addr[i+1:]
+	if len(port) == 0 {
+		err = errors.New("no port")
+		return
+	}
+	if len(ip) == 0 {
+		ip = "0.0.0.0"
+	} else {
+		if ip[0] == '[' {
+			if len(ip) < 2 || ip[len(ip)-1] != ']' {
+				err = errors.New("invalid ipv6")
+				return
+			}
+		}
+	}
+	portNum, portErr := strconv.Atoi(port)
+	if portErr != nil {
+		err = errors.New("invalid port")
+		return
+	}
+	addr = fmt.Sprintf("%s:%d", ip, portNum)
+	addrPort, err = netip.ParseAddrPort(addr)
 	return
 }
