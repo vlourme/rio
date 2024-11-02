c4d6aa18b9c0e4a26e6cd44b86171fc617d40851
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-02 21:04:04 +0800

    add loops and tls config

--- option.go
+++ option.go
@@ -1,6 +1,30 @@
 package rio
 
+import (
+	"crypto/tls"
+	"runtime"
+)
+
 type Options struct {
+	loops     int // one loop one acceptor, multi-acceptors use one promise
+	tlsConfig *tls.Config
 }
 
 type Option func(options *Options) (err error)
+
+func WithLoops(loops int) Option {
+	return func(options *Options) (err error) {
+		if loops < 1 {
+			loops = runtime.NumCPU() * 2
+		}
+		options.loops = loops
+		return
+	}
+}
+
+func WithTLSConfig(config *tls.Config) Option {
+	return func(options *Options) (err error) {
+		options.tlsConfig = config
+		return
+	}
+}
