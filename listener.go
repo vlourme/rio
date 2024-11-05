9027236d9d17b9b00c919548212507a604d36f72
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-05 20:20:57 +0800

    impl ln some fn

--- listener.go
+++ listener.go
@@ -2,25 +2,140 @@ package rio
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
 	"github.com/brickingsoft/rio/pkg/async"
+	"github.com/brickingsoft/rio/pkg/sockets"
 	"net"
 )
 
 var (
+	ErrClosed      = errors.New("rio: closed")
 	ErrBusy        = errors.New("rio: system busy")
 	ErrEmptyPacket = errors.New("rio: empty packet")
 )
-
-// Listen
-// ctx as root ctx, each conn can read it.
-func Listen(ctx context.Context, network string, addr string, options ...Option) (ln Listener, err error) {
-
-	return
-}
 
 type Listener interface {
 	Addr() (addr net.Addr)
 	Accept() (future async.Future[Connection])
 	Close() (err error)
+}
+
+// Listen
+// ctx as root ctx, each conn can read it.
+func Listen(ctx context.Context, network string, addr string, options ...Option) (ln Listener, err error) {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	opt := Options{
+		loops:        0,
+		tlsConfig:    nil,
+		multipathTCP: false,
+		proto:        0,
+		pollers:      0,
+	}
+	for _, option := range options {
+		err = option(&opt)
+		if err != nil {
+			return
+		}
+	}
+	// executors
+	executorsOptions := make([]async.Option, 0, 1)
+	if opt.maxExecutors > 0 {
+		executorsOptions = append(executorsOptions, async.MaxExecutors(opt.maxExecutors))
+	}
+	if opt.maxExecuteIdleDuration > 0 {
+		executorsOptions = append(executorsOptions, async.MaxIdleExecuteDuration(opt.maxExecuteIdleDuration))
+	}
+	executors := async.New(executorsOptions...)
+	ctx = async.With(ctx, executors)
+	// inner
+	var inner sockets.Listener
+	switch network {
+	case "tcp", "tcp4", "tcp6":
+		inner, err = sockets.ListenTCP(network, addr, sockets.Options{
+			MultipathTCP: opt.multipathTCP,
+			Proto:        opt.proto,
+			Pollers:      opt.pollers,
+		})
+		break
+	case "unix":
+		break
+	default:
+		err = errors.New("rio: network not supported")
+		break
+	}
+	if err != nil {
+		return
+	}
+	ln = &listener{
+		ctx:       ctx,
+		inner:     inner,
+		executors: executors,
+		loops:     opt.loops,
+		tlsConfig: opt.tlsConfig,
+	}
+	return
+}
+
+type listener struct {
+	ctx       context.Context
+	inner     sockets.Listener
+	executors async.Executors
+	loops     int
+	tlsConfig *tls.Config
+}
+
+func (ln *listener) Addr() (addr net.Addr) {
+	addr = ln.inner.Addr()
+	return
+}
+
+func (ln *listener) Accept() (future async.Future[Connection]) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (ln *listener) Close() (err error) {
+	err = ln.inner.Close()
+	return
+}
+
+type acceptor[R any] struct {
+	ctx   context.Context
+	ln    sockets.Listener
+	loops int
+	ch    chan R
+}
+
+func (acc *acceptor[R]) OnComplete(handler async.ResultHandler[R]) {
+	for i := 0; i < acc.loops; i++ {
+		go func(acc *acceptor[R], handler async.ResultHandler[R]) {
+			conn, ok := <-acc.ch
+			if !ok {
+				handler(acc.ctx, nil, ErrClosed)
+				return
+			}
+			handler(acc.ctx, conn, ErrEmptyPacket)
+		}(acc, handler)
+	}
+	return
+}
+
+func (acc *acceptor[R]) Await() (v R, err error) {
+	ok := false
+	v, ok = <-acc.ch
+	if !ok {
+		err = ErrClosed
+	}
+	return
+}
+
+func (acc *acceptor[R]) accept() {
+	panic("implement me")
+}
+
+func (acc *acceptor[R]) stop() {
+	panic("implement me")
 }
