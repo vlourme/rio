3e32a82e53f11fefd4c207f33c7fd4808fb9fbc3
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-06 18:29:30 +0800

    rm conn

--- connection.go
+++ connection.go
@@ -2,17 +2,15 @@ package rio
 
 import (
 	"context"
-	"errors"
 	"github.com/brickingsoft/rio/pkg/async"
 	"github.com/brickingsoft/rio/pkg/bytebufferpool"
-	"github.com/brickingsoft/rio/pkg/sockets"
 	"net"
 	"time"
 )
 
 type Inbound interface {
 	Buffer() (buf bytebufferpool.Buffer)
-	Bytes() (n int)
+	Received() (n int)
 }
 
 type inbound struct {
@@ -25,13 +23,13 @@ func (in *inbound) Buffer() (buf bytebufferpool.Buffer) {
 	return
 }
 
-func (in *inbound) Bytes() (n int) {
+func (in *inbound) Received() (n int) {
 	n = in.n
 	return
 }
 
 type Outbound interface {
-	Content() (p []byte)
+	Bytes() (p []byte)
 	Wrote() (n int)
 }
 
@@ -40,7 +38,7 @@ type outbound struct {
 	n int
 }
 
-func (out *outbound) Content() (p []byte) {
+func (out *outbound) Bytes() (p []byte) {
 	p = out.p
 	return
 }
@@ -61,160 +59,4 @@ type Connection interface {
 	Read() (future async.Future[Inbound])
 	Write(p []byte) (future async.Future[Outbound])
 	Close() (err error)
-}
-
-const (
-	defaultRWTimeout      = 15 * time.Second
-	defaultReadBufferSize = 1024
-)
-
-func newConnection(ctx context.Context, inner sockets.Connection) (conn Connection) {
-	connCtx, cancel := context.WithCancel(ctx)
-	conn = &connection{
-		ctx:    connCtx,
-		cancel: cancel,
-		inner:  inner,
-		rbs:    defaultReadBufferSize,
-		rb:     bytebufferpool.Get(),
-		rto:    defaultRWTimeout,
-		wto:    defaultRWTimeout,
-	}
-	return
-}
-
-type connection struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-	inner  sockets.Connection
-	rbs    int
-	rb     bytebufferpool.Buffer
-	rto    time.Duration
-	wto    time.Duration
-}
-
-func (conn *connection) Context() (ctx context.Context) {
-	ctx = conn.ctx
-	return
-}
-
-func (conn *connection) LocalAddr() (addr net.Addr) {
-	addr = conn.inner.LocalAddr()
-	return
-}
-
-func (conn *connection) RemoteAddr() (addr net.Addr) {
-	addr = conn.inner.RemoteAddr()
-	return
-}
-
-func (conn *connection) SetDeadline(t time.Time) (err error) {
-	timeout := time.Until(t)
-	if timeout < 1 {
-		err = errors.New("deadline too short")
-		return
-	}
-	err = conn.inner.SetDeadline(t)
-	if err != nil {
-		return
-	}
-	conn.rto = timeout
-	conn.wto = timeout
-	return
-}
-
-func (conn *connection) SetReadDeadline(t time.Time) (err error) {
-	timeout := time.Until(t)
-	if timeout < 1 {
-		err = errors.New("deadline too short")
-		return
-	}
-	err = conn.inner.SetReadDeadline(t)
-	if err != nil {
-		return
-	}
-	conn.rto = timeout
-	return
-}
-
-func (conn *connection) SetWriteDeadline(t time.Time) (err error) {
-	timeout := time.Until(t)
-	if timeout < 1 {
-		err = errors.New("deadline too short")
-		return
-	}
-	err = conn.inner.SetWriteDeadline(t)
-	if err != nil {
-		return
-	}
-	conn.wto = timeout
-	return
-}
-
-func (conn *connection) SetReadBufferSize(size int) {
-	if size < 1 {
-		size = defaultReadBufferSize
-	}
-	conn.rbs = size
-	return
-}
-
-func (conn *connection) Read() (future async.Future[Inbound]) {
-	promise, ok := async.TryPromise[Inbound](conn.ctx)
-	if !ok {
-		future = async.FailedImmediately[Inbound](conn.ctx, ErrBusy)
-		return
-	}
-	timeout := time.Now().Add(conn.rto)
-	promise.SetDeadline(timeout)
-	area := conn.rb.ApplyAreaForWrite(conn.rbs)
-	p := area.Bytes()
-	conn.inner.Read(p, func(n int, err error) {
-		area.Finish()
-		if err != nil {
-			promise.Fail(err)
-			return
-		}
-		promise.Succeed(&inbound{
-			buf: conn.rb,
-			n:   n,
-		})
-		return
-	})
-	future = promise.Future()
-	return
-}
-
-func (conn *connection) Write(p []byte) (future async.Future[Outbound]) {
-	if len(p) == 0 {
-		future = async.FailedImmediately[Outbound](conn.ctx, ErrEmptyPacket)
-		return
-	}
-	promise, ok := async.TryPromise[Outbound](conn.ctx)
-	if !ok {
-		future = async.FailedImmediately[Outbound](conn.ctx, ErrBusy)
-		return
-	}
-	timeout := time.Now().Add(conn.wto)
-	promise.SetDeadline(timeout)
-	conn.inner.Write(p, func(n int, err error) {
-		if err != nil {
-			promise.Fail(err)
-			return
-		}
-		promise.Succeed(&outbound{
-			p: p,
-			n: n,
-		})
-		return
-	})
-	future = promise.Future()
-	return
-}
-
-func (conn *connection) Close() (err error) {
-	conn.cancel()
-	err = conn.inner.Close()
-	conn.rb.Reset()
-	bytebufferpool.Put(conn.rb)
-	return
 }
