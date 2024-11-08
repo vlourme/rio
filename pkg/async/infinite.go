3546f5fd6af7f663e09702c0956256178c38aa4b
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-08 23:03:25 +0800

    add doc

--- pkg/async/infinite.go
+++ pkg/async/infinite.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+// TryInfinitePromise
+// 尝试获取一个无限流的许诺，如果资源已耗光则获取不到。
+// 无限流的特性是可以无限次完成许诺，而不是一次。
+// 但要注意，必须在不需要它后，调用 Promise.Cancel 来关闭它。
 func TryInfinitePromise[T any](ctx context.Context, buf int) (promise Promise[T], ok bool) {
 	exec := From(ctx)
 	submitter, has := exec.GetExecutorSubmitter()
@@ -16,6 +20,8 @@ func TryInfinitePromise[T any](ctx context.Context, buf int) (promise Promise[T]
 	return
 }
 
+// MustInfinitePromise
+// 必须获得一个无限许诺，如果资源已耗光则等待，直到可以或者上下文错误。
 func MustInfinitePromise[T any](ctx context.Context, buf int) (promise Promise[T], err error) {
 	times := 10
 	ok := false
--- pkg/async/promise.go
+++ pkg/async/promise.go
@@ -6,15 +6,34 @@ import (
 	"time"
 )
 
+// Promise
+// 许诺一个未来。
 type Promise[R any] interface {
+	// Complete
+	// 完成
 	Complete(result R, err error)
+	// Succeed
+	// 成功完成
 	Succeed(result R)
+	// Fail
+	// 错误完成
 	Fail(cause error)
+	// Cancel
+	// 取消许诺，未来会是一个 context.Canceled 错误。
 	Cancel()
+	// SetDeadline
+	// 设置死期。
+	// 当超时后，未来会是一个 context.DeadlineExceeded 错误。
 	SetDeadline(t time.Time)
+	// Future
+	// 返回未来
 	Future() (future Future[R])
 }
 
+// TryPromise
+// 尝试获取一个许诺，如果资源已耗光则获取不到。
+// 许诺只能完成一次，完成后则不可再用。
+// 当 Promise.Complete ，Promise.Succeed ，Promise.Fail 后，不必再 Promise.Cancel 来关闭它。
 func TryPromise[T any](ctx context.Context) (promise Promise[T], ok bool) {
 	exec := From(ctx)
 	submitter, has := exec.GetExecutorSubmitter()
@@ -25,6 +44,8 @@ func TryPromise[T any](ctx context.Context) (promise Promise[T], ok bool) {
 	return
 }
 
+// MustPromise
+// 必须获得一个许诺，如果资源已耗光则等待，直到可以或者上下文错误。
 func MustPromise[T any](ctx context.Context) (promise Promise[T], err error) {
 	times := 10
 	ok := false
