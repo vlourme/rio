dc86d73bee8bbcd2504441d5a1080c6579f3d798
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-08 18:33:09 +0800

    mod udp listener

--- pkg/sockets/udpsock_windows.go
+++ pkg/sockets/udpsock_windows.go
@@ -2,7 +2,20 @@
 
 package sockets
 
+import "net"
+
 type udpListener struct{}
 
 type udpConnection struct {
+	connection
+}
+
+func (conn *udpConnection) ReadFrom(p []byte, handler ReadFromHandler) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *udpConnection) WriteTo(p []byte, addr net.Addr, handler WriteHandler) {
+	//TODO implement me
+	panic("implement me")
 }
