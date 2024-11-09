681e35908ef7697883ff4e545bd6e4440e20d81c
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-09 17:59:39 +0800

    mod

--- pkg/sockets/udpsock_windows.go
+++ pkg/sockets/udpsock_windows.go
@@ -2,12 +2,34 @@
 
 package sockets
 
-import "net"
+import (
+	"golang.org/x/sys/windows"
+	"net"
+	"net/netip"
+)
 
-type udpListener struct{}
+func newUDPConnection(network string, family int, addr *net.UDPAddr, ipv6only bool, proto int, pollers int) (conn *udpConnection, err error) {
+	conn = &udpConnection{
+		connection: connection{},
+		cphandle:   0,
+		fd:         0,
+		addr:       nil,
+		family:     0,
+		net:        "",
+		poller:     nil,
+	}
+	conn.polling()
+	return
+}
 
 type udpConnection struct {
 	connection
+	cphandle windows.Handle
+	fd       windows.Handle
+	addr     *net.TCPAddr
+	family   int
+	net      string
+	poller   *poller
 }
 
 func (conn *udpConnection) ReadFrom(p []byte, handler ReadFromHandler) {
@@ -15,7 +37,64 @@ func (conn *udpConnection) ReadFrom(p []byte, handler ReadFromHandler) {
 	panic("implement me")
 }
 
+func (conn *udpConnection) ReadFromUDP(p []byte, handler ReadFromUDPHandler) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *udpConnection) ReadFromUDPAddrPort(p []byte, handler ReadFromUDPAddrPortHandler) {
+	//TODO implement me
+	panic("implement me")
+}
+
 func (conn *udpConnection) WriteTo(p []byte, addr net.Addr, handler WriteHandler) {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (conn *udpConnection) WriteToUDP(b []byte, addr *net.UDPAddr, handler WriteHandler) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *udpConnection) WriteToUDPAddrPort(b []byte, addr netip.AddrPort, handler WriteHandler) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *udpConnection) ReadMsgUDP(p []byte, oob []byte, handler ReadMsgUDPHandler) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *udpConnection) ReadMsgUDPAddrPort(b, oob []byte, handler ReadMsgUDPAddrPortHandler) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *udpConnection) WriteMsgUDP(b, oob []byte, addr *net.UDPAddr, handler WriteMsgHandler) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *udpConnection) WriteMsgUDPAddrPort(b, oob []byte, addr netip.AddrPort, handler WriteMsgHandler) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *udpConnection) polling() {
+	conn.poller.start()
+}
+
+func (conn *udpConnection) Close() (err error) {
+	defer func() {
+		_ = windows.WSACleanup()
+	}()
+	// stop polling
+	conn.poller.stop()
+	// close socket
+	err = conn.connection.Close()
+	// todo close iocp ?
+	_ = windows.CloseHandle(conn.cphandle)
+	return
 }
