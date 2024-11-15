b06b62478dbe2fd82d8c9fe641283c2769d43423
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-15 22:23:19 +0800

    fix

--- dialer.go
+++ dialer.go
@@ -3,6 +3,7 @@ package rio
 import (
 	"context"
 	"github.com/brickingsoft/rio/pkg/sockets"
+	"github.com/brickingsoft/rxp"
 	"github.com/brickingsoft/rxp/async"
 )
 
@@ -21,30 +22,41 @@ func Dial(ctx context.Context, network string, address string, options ...Option
 		future = async.FailedImmediately[Connection](ctx, ErrBusy)
 		return
 	}
+
 	socketOpts := sockets.Options{
 		MultipathTCP:            opts.MultipathTCP,
 		DialPacketConnLocalAddr: opts.DialPacketConnLocalAddr,
 	}
-	sockets.Dial(network, address, socketOpts, func(inner sockets.Connection, err error) {
-		if err != nil {
-			promise.Fail(err)
-			return
-		}
-		switch network {
-		case "tcp", "tcp4", "tcp6":
-			promise.Succeed(newTCPConnection(ctx, inner))
-			break
-		case "udp", "udp4", "udp6":
-			// todo
-			break
-		case "unix", "unixgram", "unixpacket":
-			// todo
-			break
-		case "ip", "ip4", "ip6":
-			// todo
-			break
-		}
+
+	executed := rxp.TryExecute(ctx, func() {
+		sockets.Dial(network, address, socketOpts, func(inner sockets.Connection, err error) {
+			if err != nil {
+				promise.Fail(err)
+				return
+			}
+			switch network {
+			case "tcp", "tcp4", "tcp6":
+				promise.Succeed(newTCPConnection(ctx, inner))
+				break
+			case "udp", "udp4", "udp6":
+				// todo
+				break
+			case "unix", "unixgram", "unixpacket":
+				// todo
+				break
+			case "ip", "ip4", "ip6":
+				// todo
+				break
+			}
+		})
 	})
+
 	future = promise.Future()
+
+	if !executed {
+		promise.Fail(ErrBusy)
+		return
+	}
+
 	return
 }
