2547f9100ad2210e56517327b5069590ebff6f75
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-15 16:06:40 +0800

    add msg test

--- pkg/sockets/packetsock_test.go
+++ pkg/sockets/packetsock_test.go
@@ -25,7 +25,12 @@ func TestListenPacket(t *testing.T) {
 		p := make([]byte, 1024)
 		conn.ReadFrom(p, func(n int, addr net.Addr, err error) {
 			t.Log("srv read:", n, addr, err, string(p[:n]))
-			wg.Done()
+			go func(conn sockets.PacketConnection) {
+				conn.WriteTo(p, addr, func(n int, err error) {
+					t.Log("srv write:", n, err)
+					wg.Done()
+				})
+			}(conn)
 		})
 	}(conn)
 
@@ -42,6 +47,69 @@ func TestListenPacket(t *testing.T) {
 		t.Error(wErr)
 	}
 	t.Log("cli write:", n)
+	p := make([]byte, 1024)
+	rn, rErr := cli.Read(p)
+	if rErr != nil {
+		t.Error(rErr)
+	}
+	t.Log("cli read:", rn, string(p[:rn]))
+	closeErr := cli.Close()
+	if closeErr != nil {
+		t.Error(closeErr)
+		return
+	}
+	wg.Wait()
+}
+
+func TestPacket_Msg(t *testing.T) {
+	conn, lnErr := sockets.ListenPacket("udp", ":9000", sockets.Options{})
+	if lnErr != nil {
+		t.Error(lnErr)
+		return
+	}
+	defer func(conn sockets.PacketConnection) {
+		closeErr := conn.Close()
+		if closeErr != nil {
+			t.Error(closeErr)
+		}
+	}(conn)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func(conn sockets.PacketConnection) {
+		p := make([]byte, 1024)
+		oob := make([]byte, 1024)
+		conn.ReadMsg(p, oob, func(n int, oobn int, flags int, addr net.Addr, err error) {
+			t.Log("srv read:", n, oobn, flags, addr, err, string(p[:n]), string(oob[:oobn]))
+			go func(conn sockets.PacketConnection) {
+				conn.WriteMsg(p[:n], oob[:oobn], addr, func(n int, oobn int, err error) {
+					t.Log("srv write:", n, oobn, err)
+					wg.Done()
+				})
+			}(conn)
+		})
+	}(conn)
+
+	addr := &net.UDPAddr{
+		IP:   net.IPv4(0, 0, 0, 0),
+		Port: 9000,
+	}
+	cli, dialErr := net.DialUDP("udp", nil, addr)
+	if dialErr != nil {
+		t.Error(dialErr)
+		return
+	}
+	n, oobn, wErr := cli.WriteMsgUDP([]byte("hello world"), []byte("oob"), nil)
+	if wErr != nil {
+		t.Error(wErr)
+	}
+	t.Log("cli write:", n, oobn)
+	p := make([]byte, 1024)
+	oob := make([]byte, 1024)
+	rn, roobn, flags, rAddr, rErr := cli.ReadMsgUDP(p, oob)
+	if rErr != nil {
+		t.Error(rErr)
+	}
+	t.Log("cli read:", rn, string(p[:rn]), roobn, string(oob[:roobn]), flags, rAddr)
 	closeErr := cli.Close()
 	if closeErr != nil {
 		t.Error(closeErr)
