271c3e1b386187c77d0fe3a35ba08ac87107023e
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-15 18:52:39 +0800

    add listen packet

--- packet.go
+++ packet.go
@@ -2,6 +2,7 @@ package rio
 
 import (
 	"context"
+	"github.com/brickingsoft/rio/pkg/sockets"
 	"github.com/brickingsoft/rio/transport"
 	"github.com/brickingsoft/rxp/async"
 	"net"
@@ -10,7 +11,26 @@ import (
 // ListenPacket
 // "udp", "udp4", "udp6", "unixgram", "ip"
 func ListenPacket(ctx context.Context, network string, addr string, options ...Option) (conn PacketConnection, err error) {
+	opts := Options{}
+	for _, o := range options {
+		err = o(&opts)
+		if err != nil {
+			return
+		}
+	}
+	socketOpts := sockets.Options{
+		MultipathTCP:            opts.MultipathTCP,
+		DialPacketConnLocalAddr: opts.DialPacketConnLocalAddr,
+	}
+	inner, innerErr := sockets.ListenPacket(network, addr, socketOpts)
+	if innerErr != nil {
+		err = innerErr
+		return
+	}
 
+	conn = &packetConnection{
+		connection: *newConnection(ctx, inner),
+	}
 	return
 }
 
@@ -21,4 +41,33 @@ type PacketConnection interface {
 	SetReadMsgUDPOOBBufferSize(size int)
 	ReadMsg() (future async.Future[transport.MsgInbound])
 	WriteMsg(p []byte, oob []byte, addr net.Addr) (future async.Future[transport.MsgOutbound])
+}
+
+type packetConnection struct {
+	connection
+}
+
+func (conn *packetConnection) ReadFrom() (future async.Future[transport.PacketInbound]) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *packetConnection) WriteTo(p []byte, addr net.Addr) (future async.Future[transport.Outbound]) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *packetConnection) SetReadMsgUDPOOBBufferSize(size int) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *packetConnection) ReadMsg() (future async.Future[transport.MsgInbound]) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (conn *packetConnection) WriteMsg(p []byte, oob []byte, addr net.Addr) (future async.Future[transport.MsgOutbound]) {
+	//TODO implement me
+	panic("implement me")
 }
