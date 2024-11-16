c84ed8a880a85ceb8092c030fe0c7b8e68c39f2f
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-16 14:49:00 +0800

    add packet test

---
+++ packet_test.go
@@ -0,0 +1,53 @@
+package rio_test
+
+import (
+	"context"
+	"github.com/brickingsoft/rio"
+	"github.com/brickingsoft/rio/transport"
+	"sync"
+	"testing"
+)
+
+func TestListenPacket(t *testing.T) {
+	ctx := context.Background()
+	srv, lnErr := rio.ListenPacket(ctx, "udp", ":9000")
+	if lnErr != nil {
+		t.Error(lnErr)
+		return
+	}
+	defer srv.Close()
+
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	srv.ReadFrom().OnComplete(func(ctx context.Context, entry transport.PacketInbound, cause error) {
+		defer wg.Done()
+		if cause != nil {
+			t.Error("srv read from:", cause)
+			return
+		}
+		p, _ := entry.Reader().Next(entry.Received())
+		t.Log("srv read from:", entry.Addr(), entry.Received(), string(p))
+
+	})
+
+	wg.Add(1)
+	rio.Dial(ctx, "udp", "127.0.0.1:9000").OnComplete(func(ctx context.Context, conn rio.Connection, cause error) {
+		defer wg.Done()
+		if cause != nil {
+			t.Error("cli read dial err:", cause)
+			return
+		}
+		wg.Add(1)
+		conn.Write([]byte("hello world")).OnComplete(func(ctx context.Context, entry transport.Outbound, cause error) {
+			defer wg.Done()
+			defer conn.Close()
+			if cause != nil {
+				t.Error("cli write err:", cause)
+				return
+			}
+			t.Log("cli write:", entry.Wrote(), entry.UnexpectedError())
+		})
+	})
+
+	wg.Wait()
+}
