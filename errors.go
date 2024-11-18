8c9b1ce02da986fccdf06ebbe01e31cf942daf56
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-18 21:30:46 +0800

    rename

--- errors.go
+++ errors.go
@@ -6,11 +6,11 @@ import (
 )
 
 var (
-	ErrClosed            = errors.New("rio: closed")
-	ErrBusy              = errors.New("rio: system busy")
-	ErrEmptyPacket       = errors.New("rio: empty packet")
-	ErrNetworkDisMatched = errors.New("rio: network is not matched")
-	ErrNilAddr           = errors.New("rio: addr is nil")
+	ErrClosed           = errors.New("rio: closed")
+	ErrBusy             = errors.New("rio: system busy")
+	ErrEmptyPacket      = errors.New("rio: empty packet")
+	ErrNetworkUnmatched = errors.New("rio: network is not matched")
+	ErrNilAddr          = errors.New("rio: addr is nil")
 )
 
 func IsClosed(err error) bool {
