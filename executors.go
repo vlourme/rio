3ebb778df9d97691f7831f526e3f4b1350092177
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-18 16:45:56 +0800

    add doc

--- executors.go
+++ executors.go
@@ -13,6 +13,12 @@ var (
 	executorsOnce sync.Once
 )
 
+// Startup
+// 启动执行器
+//
+// rio 是基于 rxp.Executors 异步编程模式。
+// 默认提供一个执行器，如果需要定制化，则使用 Startup 完成。
+// 注意：必须在程序起始位置调用，否则无效。
 func Startup(options ...rxp.Option) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -33,16 +39,30 @@ func Startup(options ...rxp.Option) (err error) {
 	return
 }
 
+// Shutdown
+// 关闭执行器
+//
+// 非优雅的，即不会等待所有协程执行完毕。
+//
+// 一般使用 ShutdownGracefully 来实现等待所有协程执行完毕。
 func Shutdown() error {
 	runtime.SetFinalizer(executors, nil)
 	return Executors().Close()
 }
 
+// ShutdownGracefully
+// 优雅的关闭执行器
+//
+// 它会等待所有协程执行完毕。
+//
+// 如果需要支持超时机制，则需要在 Startup 里进行设置。
 func ShutdownGracefully() error {
 	runtime.SetFinalizer(executors, nil)
 	return Executors().CloseGracefully()
 }
 
+// Executors
+// 获取执行器
 func Executors() rxp.Executors {
 	executorsOnce.Do(func() {
 		if executors == nil {
