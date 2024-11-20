82e063699a8869a8d0436648d8b971bf09424351
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-20 19:12:31 +0800

    add allocate err

--- errors.go
+++ errors.go
@@ -12,8 +12,13 @@ var (
 	ErrEmptyPacket      = errors.New("rio: empty packet")
 	ErrNetworkUnmatched = errors.New("rio: network is not matched")
 	ErrNilAddr          = errors.New("rio: addr is nil")
+	ErrAllocate         = errors.New("rio: allocate bytes failed")
 )
 
 func IsClosed(err error) bool {
-	return errors.Is(err, async.EOF) || errors.Is(err, async.UnexpectedEOF) || errors.Is(err, ErrClosed) || errors.Is(err, context.Canceled)
+	ok := errors.Is(err, async.EOF) || errors.Is(err, async.UnexpectedEOF) ||
+		errors.Is(err, ErrClosed) ||
+		errors.Is(err, context.Canceled) || errors.Is(err, async.UnexpectedContextFailed) ||
+		errors.Is(err, async.ExecutorsClosed)
+	return ok
 }
