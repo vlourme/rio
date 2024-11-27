8384598d7ed633b6b93df1d5ffca3e0c399afeda
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-27 23:08:45 +0800

    add empty bytes

--- pkg/aio/aio.go
+++ pkg/aio/aio.go
@@ -5,6 +5,7 @@ import "errors"
 var (
 	ErrUnexpectedCompletion      = errors.New("aio: unexpected completion error")
 	ErrOperationDeadlineExceeded = errors.New("aio: operation deadline exceeded")
+	ErrEmptyBytes                = errors.New("aio: empty bytes")
 )
 
 func IsUnexpectedCompletionError(err error) bool {
