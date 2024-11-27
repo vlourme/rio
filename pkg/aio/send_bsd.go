e0ce86cdb3d0be84b9eb9cc7cf3985f2bb6c26cc
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-27 21:03:00 +0800

    add send

---
+++ pkg/aio/send_bsd.go
@@ -0,0 +1,33 @@
+//go:build darwin || dragonfly || freebsd || netbsd || openbsd
+
+package aio
+
+func Send(fd NetFd, b []byte, cb OperationCallback) {
+
+	return
+}
+
+func completeSend(result int, op *Operator, err error) {
+
+	return
+}
+
+func SendTo(fd NetFd, b []byte, addr net.Addr, cb OperationCallback) {
+
+	return
+}
+
+func completeSendTo(result int, op *Operator, err error) {
+
+	return
+}
+
+func SendMsg(fd NetFd, b []byte, oob []byte, addr net.Addr, cb OperationCallback) {
+
+	return
+}
+
+func completeSendMsg(result int, op *Operator, err error) {
+
+	return
+}
---
+++ pkg/aio/send_windows.go
@@ -0,0 +1,35 @@
+//go:build windows
+
+package aio
+
+import "net"
+
+func Send(fd NetFd, b []byte, cb OperationCallback) {
+
+	return
+}
+
+func completeSend(result int, op *Operator, err error) {
+
+	return
+}
+
+func SendTo(fd NetFd, b []byte, addr net.Addr, cb OperationCallback) {
+
+	return
+}
+
+func completeSendTo(result int, op *Operator, err error) {
+
+	return
+}
+
+func SendMsg(fd NetFd, b []byte, oob []byte, addr net.Addr, cb OperationCallback) {
+
+	return
+}
+
+func completeSendMsg(result int, op *Operator, err error) {
+
+	return
+}
