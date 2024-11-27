aa9a80594d41b4afe485d57bcaa7c050475b6f1c
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-27 20:45:16 +0800

    mod listen ip

--- pkg/aio/listen.go
+++ pkg/aio/listen.go
@@ -14,7 +14,7 @@ type ListenerOptions struct {
 func Listen(network string, address string, opts ListenerOptions) (fd NetFd, err error) {
 	addr, family, _, addrErr := ResolveAddr(network, address)
 	if addrErr != nil {
-		err = &net.OpError{Op: "listen", Net: network, Source: nil, Addr: nil, Err: addrErr}
+		err = addrErr
 		return
 	}
 
@@ -24,22 +24,29 @@ func Listen(network string, address string, opts ListenerOptions) (fd NetFd, err
 		if opts.MultipathTCP {
 			proto = tryGetMultipathTCPProto()
 		}
-		fd, err = newNetFd(network, family, syscall.SOCK_STREAM, proto, addr, nil, nil)
-		//fd, err = newListener(network, family, addr, proto)
+		fd, err = newListenerFd(network, family, syscall.SOCK_STREAM, proto, addr, nil)
 		break
 	case "udp", "udp4", "udp6":
-		fd, err = newNetFd(network, family, syscall.SOCK_DGRAM, 0, addr, nil, opts.MulticastInterface)
+		fd, err = newListenerFd(network, family, syscall.SOCK_DGRAM, 0, addr, opts.MulticastInterface)
 		break
 	case "unix":
-		fd, err = newNetFd(network, family, syscall.SOCK_STREAM, 0, addr, nil, nil)
+		fd, err = newListenerFd(network, family, syscall.SOCK_STREAM, 0, addr, nil)
 		break
 	case "unixgram":
-		fd, err = newNetFd(network, family, syscall.SOCK_DGRAM, 0, addr, nil, nil)
+		fd, err = newListenerFd(network, family, syscall.SOCK_DGRAM, 0, addr, nil)
 		break
 	case "unixpacket":
-		fd, err = newNetFd(network, family, syscall.SOCK_SEQPACKET, 0, addr, nil, nil)
+		fd, err = newListenerFd(network, family, syscall.SOCK_SEQPACKET, 0, addr, nil)
 		break
 	case "ip", "ip4", "ip6":
+		proto := 0
+		var parseProtoError error
+		network, proto, parseProtoError = ParseIpProto(network)
+		if parseProtoError != nil {
+			err = parseProtoError
+			return
+		}
+		fd, err = newListenerFd(network, family, syscall.SOCK_RAW, proto, addr, nil)
 		break
 	default:
 		err = errors.New("aio.Listen: network is not support")
