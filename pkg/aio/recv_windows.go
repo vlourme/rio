a7a54acd90d1aa34c45782546cf85d8c0118a04c
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-27 21:02:52 +0800

    add recv

---
+++ pkg/aio/recv_windows.go
@@ -0,0 +1,33 @@
+//go:build windows
+
+package aio
+
+func Recv(fd NetFd, b []byte, cb OperationCallback) {
+
+	return
+}
+
+func completeRecv(result int, op *Operator, err error) {
+
+	return
+}
+
+func RecvFrom(fd NetFd, b []byte, cb OperationCallback) {
+
+	return
+}
+
+func completeRecvFrom(result int, op *Operator, err error) {
+
+	return
+}
+
+func RecvMsg(fd NetFd, b []byte, oob []byte, cb OperationCallback) {
+
+	return
+}
+
+func completeRecvMsg(result int, op *Operator, err error) {
+
+	return
+}
