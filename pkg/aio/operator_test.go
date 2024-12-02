5ec5e979ed9667d9cae0042e27f57e89686ce5c3
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-02 20:03:04 +0800

    mod

--- pkg/aio/operator_test.go
+++ pkg/aio/operator_test.go
@@ -1,1 +1,14 @@
 package aio_test
+
+import (
+	"github.com/brickingsoft/rio/pkg/aio"
+	"testing"
+	"unsafe"
+)
+
+func TestOperatorPtr(t *testing.T) {
+	o1 := new(aio.Operator)
+	p1 := uint64(uintptr(unsafe.Pointer(o1)))
+	o2 := (*aio.Operator)(unsafe.Pointer(uintptr(p1)))
+	t.Log(unsafe.Pointer(o1), unsafe.Pointer(o2), unsafe.Pointer(o1) == unsafe.Pointer(o2))
+}
