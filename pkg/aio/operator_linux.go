cc9fc240fe639a4472cf6fb37289c7ad5d1b833a
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-03 20:59:35 +0800

    mod canceler

--- pkg/aio/operator_linux.go
+++ pkg/aio/operator_linux.go
@@ -2,7 +2,10 @@
 
 package aio
 
-import "time"
+import (
+	"time"
+	"unsafe"
+)
 
 type Operator struct {
 	fd         Fd
@@ -15,20 +18,23 @@ type Operator struct {
 
 type operatorCanceler struct {
 	cylinder *IOURingCylinder
-	userdata uint64
+	op       *Operator
 }
 
-func (op *operatorCanceler) Cancel() {
-	ring := op.cylinder.ring
+func (canceler *operatorCanceler) Cancel() {
+	cylinder := canceler.cylinder
+	ring := cylinder.ring
+	op := canceler.op
+	opPtr := uint64(uintptr(unsafe.Pointer(op)))
 	for {
 		entry := ring.GetSQE()
 		if entry == nil {
-			if op.cylinder.stopped.Load() {
+			if cylinder.stopped.Load() {
 				return
 			}
 			continue
 		}
-		entry.prepareRW(opAsyncCancel, -1, 0, 0, 0, op.userdata, 0)
+		entry.prepareRW(opAsyncCancel, -1, 0, 0, 0, opPtr, 0)
 		break
 	}
 }
