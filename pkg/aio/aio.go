c16b4aadafbe398cc2ae5811c7b43b38d49b2a3b
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-03 15:21:28 +0800

    add err check

--- pkg/aio/aio.go
+++ pkg/aio/aio.go
@@ -6,8 +6,21 @@ var (
 	ErrUnexpectedCompletion      = errors.New("aio: unexpected completion error")
 	ErrOperationDeadlineExceeded = errors.New("aio: operation deadline exceeded")
 	ErrEmptyBytes                = errors.New("aio: empty bytes")
+	ErrBusy                      = errors.New("aio: busy")
 )
 
 func IsUnexpectedCompletionError(err error) bool {
 	return errors.Is(err, ErrUnexpectedCompletion)
+}
+
+func IsOperationDeadlineExceededError(err error) bool {
+	return errors.Is(err, ErrOperationDeadlineExceeded)
+}
+
+func IsEmptyBytesError(err error) bool {
+	return errors.Is(err, ErrEmptyBytes)
+}
+
+func IsBusyError(err error) bool {
+	return errors.Is(err, ErrBusy)
 }
