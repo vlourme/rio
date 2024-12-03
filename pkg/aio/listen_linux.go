f2e3bef648b5cc82e69cb38b96036eb943b35862
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-03 15:26:38 +0800

    add listen and accept

--- pkg/aio/listen_linux.go
+++ pkg/aio/listen_linux.go
@@ -4,23 +4,229 @@ package aio
 
 import (
 	"bufio"
+	"errors"
 	"net"
 	"os"
 	"strconv"
 	"strings"
 	"syscall"
 	"time"
+	"unsafe"
 )
 
 func newListenerFd(network string, family int, sotype int, proto int, addr net.Addr, multicastInterface *net.Interface) (v *netFd, err error) {
+	// create sock
+	sock, sockErr := newSocket(family, sotype, proto)
+	if sockErr != nil {
+		err = sockErr
+		return
+	}
+	switch sotype {
+	case syscall.SOCK_STREAM, syscall.SOCK_SEQPACKET:
+		setOptErr := setDefaultListenerSocketOpts(sock)
+		if setOptErr != nil {
+			_ = syscall.Close(sock)
+			err = setOptErr
+			return
+		}
+		// bind
+		sa := AddrToSockaddr(addr)
+		bindErr := syscall.Bind(sock, sa)
+		if bindErr != nil {
+			_ = syscall.Close(sock)
+			err = os.NewSyscallError("bind", bindErr)
+			return
+		}
+		// listen
+		listenErr := syscall.Listen(sock, syscall.SOMAXCONN)
+		if listenErr != nil {
+			_ = syscall.Close(sock)
+			err = os.NewSyscallError("listen", listenErr)
+			return
+		}
+		// lsa
+		lsa, getLsaErr := syscall.Getsockname(sock)
+		if getLsaErr != nil {
+			_ = syscall.Close(sock)
+			err = os.NewSyscallError("getsockname", getLsaErr)
+			return
+		}
+		addr = SockaddrToAddr(network, lsa)
+		break
+	case syscall.SOCK_DGRAM:
+		isListenMulticastUDP := false
+		var gaddr *net.UDPAddr
+		udpAddr, isUdpAddr := addr.(*net.UDPAddr)
+		if isUdpAddr {
+			if udpAddr.IP != nil && udpAddr.IP.IsMulticast() {
+				isListenMulticastUDP = true
+				gaddr = udpAddr
+				localUdpAddr := *udpAddr
+				switch family {
+				case syscall.AF_INET:
+					localUdpAddr.IP = net.IPv4zero.To4()
+				case syscall.AF_INET6:
+					localUdpAddr.IP = net.IPv6zero
+				}
+				addr = &localUdpAddr
+			}
+		}
+		// listen multicast udp
+		if isListenMulticastUDP {
+			if ip4 := gaddr.IP.To4(); ip4 != nil {
+				if multicastInterface != nil {
+					if err = setIPv4MulticastInterface(sock, multicastInterface); err != nil {
+						_ = syscall.Close(sock)
+						return
+					}
+				}
+				if err = setIPv4MulticastLoopback(sock, false); err != nil {
+					_ = syscall.Close(sock)
+					return
+				}
+				if err = joinIPv4Group(sock, multicastInterface, ip4); err != nil {
+					_ = syscall.Close(sock)
+					return
+				}
+			} else {
+				if multicastInterface != nil {
+					if err = setIPv6MulticastInterface(sock, multicastInterface); err != nil {
+						_ = syscall.Close(sock)
+						return
+					}
+				}
+				if err = setIPv6MulticastLoopback(sock, false); err != nil {
+					_ = syscall.Close(sock)
+					return
+				}
+				if err = joinIPv6Group(sock, multicastInterface, gaddr.IP); err != nil {
+					_ = syscall.Close(sock)
+					return
+				}
+			}
+		}
+		// bind
+		sa := AddrToSockaddr(addr)
+		bindErr := syscall.Bind(sock, sa)
+		if bindErr != nil {
+			err = os.NewSyscallError("bind", bindErr)
+			_ = syscall.Close(sock)
+			return
+		}
+		break
+	case syscall.SOCK_RAW:
+		// todo
+		break
+	default:
+		break
+	}
 
-	// syscall.Listen(0, somaxconn)
+	// fd
+	nfd := &netFd{
+		handle:     sock,
+		network:    network,
+		family:     family,
+		socketType: sotype,
+		protocol:   proto,
+		localAddr:  addr,
+		remoteAddr: nil,
+		rop:        Operator{},
+		wop:        Operator{},
+	}
+	nfd.rop.fd = nfd
+	nfd.wop.fd = nfd
 
+	v = nfd
 	return
 }
 
 func Accept(fd NetFd, cb OperationCallback) {
+	// op
+	op := fd.ReadOperator()
+	// ring
+	cylinder := nextIOURingCylinder()
+	entry := cylinder.ring.GetSQE()
+	if entry == nil {
+		cb(0, op.userdata, ErrBusy)
+		return
+	}
 
+	// cb
+	op.callback = cb
+	// completion
+	op.completion = completeAccept
+
+	// ln
+	lnFd := fd.Fd()
+	// addr
+	op.userdata.Msg.Name = new(syscall.RawSockaddrAny)
+	op.userdata.Msg.Namelen = syscall.SizeofSockaddrAny
+	addrPtr := uintptr(unsafe.Pointer(op.userdata.Msg.Name))
+	addrLenPtr := uint64(uintptr(unsafe.Pointer(&op.userdata.Msg.Namelen)))
+
+	// userdata
+	userdata := uint64(uintptr(unsafe.Pointer(&op)))
+	entry.prepareRW(opAccept, lnFd, addrPtr, 0, addrLenPtr, userdata, 0)
+	// submit
+	_, submitErr := cylinder.ring.Submit()
+	if submitErr != nil {
+		cb(0, op.userdata, errors.Join(errors.New("aio: accept failed"), submitErr))
+		return
+	}
+	return
+}
+
+func completeAccept(result int, op *Operator, err error) {
+	// cb
+	cb := op.callback
+	// userdata
+	userdata := op.userdata
+	if err != nil {
+		cb(result, userdata, os.NewSyscallError("io_uring_prep_accept", err))
+		return
+	}
+	// conn
+	connFd := result
+	// ln
+	ln, _ := op.fd.(NetFd)
+	// addr
+	// get local addr
+	lsa, lsaErr := syscall.Getsockname(connFd)
+	if lsaErr != nil {
+		_ = syscall.Close(connFd)
+		op.callback(result, userdata, os.NewSyscallError("getsockname", lsaErr))
+		return
+	}
+	la := SockaddrToAddr(ln.Network(), lsa)
+
+	// get remote addr
+	rsa, rsaErr := syscall.Getpeername(connFd)
+	if rsaErr != nil {
+		_ = syscall.Close(connFd)
+		op.callback(result, userdata, os.NewSyscallError("getsockname", rsaErr))
+		return
+	}
+	ra := SockaddrToAddr(ln.Network(), rsa)
+
+	// conn
+	conn := &netFd{
+		handle:     connFd,
+		network:    ln.Network(),
+		family:     ln.Family(),
+		socketType: ln.SocketType(),
+		protocol:   ln.Protocol(),
+		localAddr:  la,
+		remoteAddr: ra,
+		rop:        Operator{},
+		wop:        Operator{},
+	}
+	conn.rop.fd = conn
+	conn.wop.fd = conn
+
+	userdata.Fd = conn
+	// cb
+	cb(connFd, userdata, nil)
+	return
 }
 
 func SetReadBuffer(fd NetFd, n int) (err error) {
