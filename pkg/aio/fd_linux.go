018a21034d99d2949a460699c081659fce3ae62c
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-03 16:38:07 +0800

    mod close

--- pkg/aio/fd_linux.go
+++ pkg/aio/fd_linux.go
@@ -4,25 +4,33 @@ package aio
 
 import (
 	"errors"
-	"syscall"
+	"unsafe"
 )
 
 func Close(fd Fd, cb OperationCallback) {
-	switch fd.(type) {
-	case NetFd:
-		// todo: use ring.close
-		handle := fd.Fd()
-		err := syscall.Close(handle)
-		if err != nil {
-			err = errors.Join(errors.New("aio.Operator: close failed"), err)
-		}
-		cb(handle, fd.WriteOperator().userdata, err)
-		return
-	case FileFd:
-		cb(0, fd.WriteOperator().userdata, errors.New("aio.Operator: close was not supported"))
-		return
-	default:
-		cb(0, fd.WriteOperator().userdata, errors.New("aio.Operator: close was not supported"))
+	op := fd.WriteOperator()
+
+	cylinder := nextIOURingCylinder()
+	entry := cylinder.ring.GetSQE()
+	if entry == nil {
+		cb(0, op.userdata, ErrBusy)
 		return
 	}
+
+	op.callback = cb
+	op.completion = completeClose
+
+	userdata := uint64(uintptr(unsafe.Pointer(&op)))
+
+	entry.prepareRW(opClose, fd.Fd(), 0, 0, 0, userdata, 0)
+}
+
+func completeClose(result int, op *Operator, err error) {
+	if err != nil {
+		err = errors.Join(errors.New("aio.Operator: close failed"), err)
+		op.callback(result, op.userdata, err)
+		return
+	}
+	op.callback(result, op.userdata, nil)
+	return
 }
