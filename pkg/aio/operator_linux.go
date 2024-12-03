2df9cb7a5b273f64c931b8392cced94d64530937
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-03 15:26:05 +0800

    add op canceler

--- pkg/aio/operator_linux.go
+++ pkg/aio/operator_linux.go
@@ -11,13 +11,25 @@ type Operator struct {
 	completion OperatorCompletion
 	timeout    time.Duration
 	timer      *operatorTimer
-	cylinder   Cylinder
 }
 
-func (op *Operator) Begin() {
-	op.cylinder.Up()
+type operatorCanceler struct {
+	cylinder *IOURingCylinder
+	userdata uint64
 }
 
-func (op *Operator) Finish() {
-	op.cylinder.Down()
+func (op *operatorCanceler) Cancel() {
+	ring := op.cylinder.ring
+	for {
+		entry := ring.GetSQE()
+		if entry == nil {
+			if op.cylinder.stopped.Load() {
+				return
+			}
+			continue
+		}
+		entry.prepareRW(opAsyncCancel, -1, 0, 0, 0, op.userdata, 0)
+		_, _ = ring.Submit()
+		break
+	}
 }
