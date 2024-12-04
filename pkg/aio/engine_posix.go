09c2758b8cfed7dceb87c29d0238f2f2edf4f2c5
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-04 15:59:16 +0800

    add

--- pkg/aio/engine_posix.go
+++ pkg/aio/engine_posix.go
@@ -1,3 +1,16 @@
 //go:build !linux && !windows && !bsd
 
 package aio
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func (engine *Engine) Start() {
+	panic(fmt.Errorf("aio: %s is not supported", runtime.GOOS))
+}
+
+func (engine *Engine) Stop() {
+	panic(fmt.Errorf("aio: %s is not supported", runtime.GOOS))
+}
