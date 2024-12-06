cdc4bad9de156fdd3ba2691d03e391cdfe7d7091
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-06 16:15:52 +0800

    mod default setup flags and feats

--- pkg/aio/engine_linux.go
+++ pkg/aio/engine_linux.go
@@ -50,30 +50,58 @@ func (engine *Engine) Start() {
 			if settings.Param.Flags&SetupSQPoll == 0 {
 				settings.Param.Flags = settings.Param.Flags | SetupSQPoll
 			}
-			if compareKernelVersion(major, minor, 6, 0) >= 0 {
-				// single issuer
-				if settings.Param.Flags&SetupSingleIssuer == 0 {
-					settings.Param.Flags = settings.Param.Flags | SetupSingleIssuer
+			if compareKernelVersion(major, minor, 5, 18) >= 0 {
+				// submit all
+				if settings.Param.Flags&SetupSubmitAll == 0 {
+					settings.Param.Flags = settings.Param.Flags | SetupSubmitAll
 				}
-				if compareKernelVersion(major, minor, 6, 1) >= 0 {
-					// defer task run
-					if settings.Param.Flags&SetupDeferTaskRun == 0 {
-						settings.Param.Flags = settings.Param.Flags | SetupDeferTaskRun
+				if compareKernelVersion(major, minor, 6, 0) >= 0 {
+					// single issuer
+					if settings.Param.Flags&SetupSingleIssuer == 0 {
+						settings.Param.Flags = settings.Param.Flags | SetupSingleIssuer
+					}
+					if compareKernelVersion(major, minor, 6, 1) >= 0 {
+						// defer task run
+						if settings.Param.Flags&SetupDeferTaskRun == 0 {
+							settings.Param.Flags = settings.Param.Flags | SetupDeferTaskRun
+						}
 					}
 				}
 			}
 		}
 	}
 	if settings.Param.Features == 0 {
-		if compareKernelVersion(major, minor, 5, 5) >= 0 {
-			// submit stable
-			if settings.Param.Features&FeatSubmitStable == 0 {
-				settings.Param.Features = settings.Param.Features | FeatSubmitStable
+		if compareKernelVersion(major, minor, 5, 4) >= 0 {
+			// single mmap
+			if settings.Param.Features&FeatSingleMMap == 0 {
+				settings.Param.Features = settings.Param.Features | FeatSingleMMap
 			}
-			if compareKernelVersion(major, minor, 5, 11) >= 0 {
-				// ext arg
-				if settings.Param.Features&FeatExtArg == 0 {
-					settings.Param.Features = settings.Param.Features | FeatExtArg
+			if compareKernelVersion(major, minor, 5, 5) >= 0 {
+				// submit stable
+				if settings.Param.Features&FeatSubmitStable == 0 {
+					settings.Param.Features = settings.Param.Features | FeatSubmitStable
+				}
+				if compareKernelVersion(major, minor, 5, 7) >= 0 {
+					// fast poll
+					if settings.Param.Features&FeatFastPoll == 0 {
+						settings.Param.Features = settings.Param.Features | FeatFastPoll
+					}
+					if compareKernelVersion(major, minor, 5, 11) >= 0 {
+						// ext arg
+						if settings.Param.Features&FeatExtArg == 0 {
+							settings.Param.Features = settings.Param.Features | FeatExtArg
+						}
+						// non fixed
+						if settings.Param.Features&FeatSQPollNonfixed == 0 {
+							settings.Param.Features = settings.Param.Features | FeatSQPollNonfixed
+						}
+						if compareKernelVersion(major, minor, 5, 12) >= 0 {
+							// native workers
+							if settings.Param.Features&FeatNativeWorkers == 0 {
+								settings.Param.Features = settings.Param.Features | FeatNativeWorkers
+							}
+						}
+					}
 				}
 			}
 		}
@@ -654,7 +682,7 @@ func (ring *IOURing) queueMmap(fd int, param *IOURingSetupParam) (err error) {
 	}
 	sq.sqes = (*SubmissionQueueEntry)(unsafe.Pointer(ringPtr))
 
-	// setup queue
+	// setup squeue
 	sq.head = (*uint32)(unsafe.Pointer(uintptr(sq.ringPtr) + uintptr(param.SQOff.Head)))
 	sq.tail = (*uint32)(unsafe.Pointer(uintptr(sq.ringPtr) + uintptr(param.SQOff.Tail)))
 	sq.ringMask = (*uint32)(unsafe.Pointer(uintptr(sq.ringPtr) + uintptr(param.SQOff.RingMask)))
@@ -662,7 +690,7 @@ func (ring *IOURing) queueMmap(fd int, param *IOURingSetupParam) (err error) {
 	sq.flags = (*uint32)(unsafe.Pointer(uintptr(sq.ringPtr) + uintptr(param.SQOff.Flags)))
 	sq.dropped = (*uint32)(unsafe.Pointer(uintptr(sq.ringPtr) + uintptr(param.SQOff.Dropped)))
 	sq.array = (*uint32)(unsafe.Pointer(uintptr(sq.ringPtr) + uintptr(param.SQOff.Array)))
-
+	// setup cqueue
 	cq.head = (*uint32)(unsafe.Pointer(uintptr(cq.ringPtr) + uintptr(param.CQOff.Head)))
 	cq.tail = (*uint32)(unsafe.Pointer(uintptr(cq.ringPtr) + uintptr(param.CQOff.Tail)))
 	cq.ringMask = (*uint32)(unsafe.Pointer(uintptr(cq.ringPtr) + uintptr(param.CQOff.RingMask)))
@@ -691,39 +719,32 @@ func (ring *IOURing) sqNeedsEnter(submit uint32, flags *uint32) bool {
 	if submit == 0 {
 		return false
 	}
-
 	if (ring.flags & SetupSQPoll) == 0 {
 		return true
 	}
-
 	if atomic.LoadUint32(ring.sq.flags)&SQNeedWakeup != 0 {
 		*flags |= EnterSQWakeup
 
 		return true
 	}
-
 	return false
 }
 
 func (ring *IOURing) flushSQ() uint32 {
 	sq := ring.sq
 	tail := sq.sqeTail
-
 	if sq.sqeHead != tail {
 		sq.sqeHead = tail
 		atomic.StoreUint32(sq.tail, tail)
 	}
-
 	return tail - atomic.LoadUint32(sq.head)
 }
 
 func (ring *IOURing) sqReady() uint32 {
 	khead := *ring.sq.head
-
 	if ring.flags&SetupSQPoll != 0 {
 		khead = atomic.LoadUint32(ring.sq.head)
 	}
-
 	return ring.sq.sqeTail - khead
 }
 
@@ -745,7 +766,6 @@ func (ring *IOURing) cqeShift() uint32 {
 	if ring.flags&SetupCQE32 != 0 {
 		return 1
 	}
-
 	return 0
 }
 
@@ -763,7 +783,6 @@ func (ring *IOURing) cqNeedsFlush() bool {
 
 func (ring *IOURing) privateGetCQE(data *getData) (cqe *CompletionQueueEvent, err error) {
 	var looped bool
-
 	for {
 		var needEnter bool
 		var flags uint32
@@ -1035,7 +1054,6 @@ const (
 	opUringCmd
 	opSendZC
 	opSendMsgZC
-	opLast
 )
 
 type SubmissionQueueEntry struct {
@@ -1155,7 +1173,8 @@ const (
 	// 使用 struct io_uring_params.cq_entries 条目创建完成队列。值必须大于条目数，并可四舍五入为下一个 2 的幂次。
 	SetupCQSize
 	// SetupClamp
-	// 如果指定了该标志，且条目数超过 IORING_MAX_ENTRIES，那么条目数将被箝位在 IORING_MAX_ENTRIES。如果设置了标志 IORING_SETUP_CQSIZE，且 struct io_uring_params.cq_entries 的值超过了 IORING_MAX_CQ_ENTRIES，则将以 IORING_MAX_CQ_ENTRIES 的值箝位。
+	// 如果指定了该标志，且条目数超过 IORING_MAX_ENTRIES，那么条目数将被箝位在 IORING_MAX_ENTRIES。
+	// 如果设置了标志 IORING_SETUP_CQSIZE，且 struct io_uring_params.cq_entries 的值超过了 IORING_MAX_CQ_ENTRIES，则将以 IORING_MAX_CQ_ENTRIES 的值箝位。
 	SetupClamp
 	// SetupAttachWQ
 	// 设置该标志时，应同时将 struct io_uring_params.wq_fd 设置为现有的 io_uring ring 文件描述符。
@@ -1228,7 +1247,8 @@ const (
 
 const (
 	// FeatSingleMMap
-	// 如果设置了该标志，则只需调用一次 mmap(2)，即可映射两个 SQ 和 CQ 环。SQE 仍需单独分配。这样，所需的 mmap(2) 调用次数就从三次减少到两次。自内核 5.4 起可用。
+	// 如果设置了该标志，则只需调用一次 mmap(2)，即可映射两个 SQ 和 CQ 环。
+	// SQE 仍需单独分配。这样，所需的 mmap(2) 调用次数就从三次减少到两次。自内核 5.4 起可用。
 	FeatSingleMMap uint32 = 1 << iota
 	// FeatNoDrop
 	// 如果设置了这个标志，io_uring 就几乎不会丢弃完成事件。只有当内核内存耗尽时，才会发生丢弃事件，
@@ -1244,13 +1264,23 @@ const (
 	// 如果设置了该标志，应用程序就可以确定，当内核消耗 SQE 时，任何用于异步卸载的数据都已消耗完毕。自内核 5.5 起可用。
 	FeatSubmitStable
 	// FeatRWCurPos
-	// 如果设置了这个标志，应用程序就可以在 IORING_OP_{READV,WRITEV}、IORING_OP_{READ,WRITE}_FIXED 和 IORING_OP_{READ,WRITE} 中指定偏移量 == -1 表示当前文件位置，其行为与偏移量 == -1 的 preadv2(2) 和 pwritev2(2) 类似。 它将使用（并更新）当前文件位置。这显然需要注意的是，如果应用程序在运行过程中进行了多次读取或写入，那么最终结果将不尽如人意。这与线程共享文件描述符并使用当前文件位置进行 IO 的情况类似。自内核 5.6 起可用。
+	// 如果设置了这个标志，应用程序就可以在 IORING_OP_{READV,WRITEV}、IORING_OP_{READ,WRITE}_FIXED 和 IORING_OP_{READ,WRITE} 中指定偏移量 == -1 表示当前文件位置，
+	// 其行为与偏移量 == -1 的 preadv2(2) 和 pwritev2(2) 类似。
+	// 它将使用（并更新）当前文件位置。
+	// 这显然需要注意的是，如果应用程序在运行过程中进行了多次读取或写入，那么最终结果将不尽如人意。
+	// 这与线程共享文件描述符并使用当前文件位置进行 IO 的情况类似。自内核 5.6 起可用。
 	FeatRWCurPos
 	// FeatCurPersonality
-	// 如果设置了这个标志，那么 io_uring 将保证同步和异步执行请求时，都使用调用 io_uring_enter(2) 对请求进行排队的任务的凭据。如果未设置该标记，则会使用最初注册 io_uring 的任务的凭据发出请求。如果只有一个任务在使用一个环，那么这个标志并不重要，因为凭据始终是相同的。请注意，这是默认行为，任务仍然可以通过 io_uring_register(2) 以 IORING_REGISTER_PERSONALITY 注册不同的个性，并在 sqe 中指定要使用的个性。自内核 5.6 起可用。
+	// 如果设置了这个标志，那么 io_uring 将保证同步和异步执行请求时，都使用调用 io_uring_enter(2) 对请求进行排队的任务的凭据。
+	// 如果未设置该标记，则会使用最初注册 io_uring 的任务的凭据发出请求。
+	// 如果只有一个任务在使用一个环，那么这个标志并不重要，因为凭据始终是相同的。
+	// 请注意，这是默认行为，任务仍然可以通过 io_uring_register(2) 以 IORING_REGISTER_PERSONALITY 注册不同的个性，并在 sqe 中指定要使用的个性。自内核 5.6 起可用。
 	FeatCurPersonality
 	// FeatFastPoll
-	// 如果设置了这个标志，那么 io_uring 将支持使用内部轮询机制来驱动数据/空间就绪。这意味着无法读取或写入文件数据的请求不再需要交由异步线程处理，而是会在文件就绪时开始运行。这类似于在用户空间进行轮询 + 读/写操作，但无需这样做。如果设置了该标记，等待空间/数据的请求就不会阻塞线程，从而减少了资源消耗。自内核 5.7 起可用。
+	// 如果设置了这个标志，那么 io_uring 将支持使用内部轮询机制来驱动数据/空间就绪。
+	// 这意味着无法读取或写入文件数据的请求不再需要交由异步线程处理，而是会在文件就绪时开始运行。
+	// 这类似于在用户空间进行轮询 + 读/写操作，但无需这样做。
+	// 如果设置了该标记，等待空间/数据的请求就不会阻塞线程，从而减少了资源消耗。自内核 5.7 起可用。
 	FeatFastPoll
 	// FeatPoll32Bits
 	// 如果设置了该标志，IORING_OP_POLL_ADD 命令将接受全部 32 位的基于 epoll 的标志。最值得注意的是 EPOLLEXCLUSIVE，它允许独占（唤醒单个等待者）行为。自内核 5.9 起可用。
@@ -1279,26 +1309,31 @@ const (
 	//如果在 enter 系统调用的标志中设置了 IORING_ENTER_EXT_ARG，则必须传入指向该结构的指针。自内核 5.11 起可用。
 	FeatExtArg
 	// FeatNativeWorkers
-	// 如果设置了这个标志，那么 io_uring 将使用本地 Worker 作为异步助手。以前的内核使用的内核线程会假定原始 io_uring 拥有任务的身份，但以后的内核会主动创建看起来更像普通进程的线程。自内核 5.12 起可用。
+	// 如果设置了这个标志，那么 io_uring 将使用本地 Worker 作为异步助手。
+	// 以前的内核使用的内核线程会假定原始 io_uring 拥有任务的身份，但以后的内核会主动创建看起来更像普通进程的线程。
+	// 自内核 5.12 起可用。
 	FeatNativeWorkers
 	// FeatRcrcTags
-	// 如果设置了这个标志，那么 io_uring 将支持与固定文件和缓冲区相关的各种功能。尤其是，它表明已注册的缓冲区可以就地更新，而在此之前，必须先取消注册整个缓冲区。自内核 5.13 起可用。
-	FeatRcrcTags
+	// 如果设置了这个标志，那么 io_uring 将支持与固定文件和缓冲区相关的各种功能。
+	// 尤其是，它表明已注册的缓冲区可以就地更新，而在此之前，必须先取消注册整个缓冲区。自内核 5.13 起可用。
+	_FeatRcrcTags
 	// FeatCQESkip
 	// 如果设置了该标志，io_uring 就支持在提交的 SQE 中设置 IOSQE_CQE_SKIP_SUCCESS，表明如果正常执行，就不会为该 SQE 生成 CQE。
 	// 如果在处理 SQE 时发生错误，仍会生成带有相应错误值的 CQE。自内核 5.17 起可用。
-	FeatCQESkip
+	_FeatCQESkip
 	// FeatLinkedFile
 	// 如果设置了这个标志，那么 io_uring 将支持为有依赖关系的 SQE 合理分配文件。
-	// 例如，如果使用 IOSQE_IO_LINK 提交了一连串 SQE，那么没有该标志的内核将为每个链接预先准备文件。如果前一个链接打开了一个已知索引的文件，例如使用直接描述符打开或接受，那么文件分配就需要在执行该 SQE 后进行。如果设置了该标志，内核将推迟文件分配，直到开始执行给定请求。自内核 5.17 起可用。
+	// 例如，如果使用 IOSQE_IO_LINK 提交了一连串 SQE，那么没有该标志的内核将为每个链接预先准备文件。
+	// 如果前一个链接打开了一个已知索引的文件，例如使用直接描述符打开或接受，那么文件分配就需要在执行该 SQE 后进行。
+	// 如果设置了该标志，内核将推迟文件分配，直到开始执行给定请求。自内核 5.17 起可用。
 	FeatLinkedFile
 	// FeatRegRegRing
 	// 如果设置了该标志，则 io_uring 支持通过 IORING_REGISTER_USE_REGISTERED_RING，使用注册环 fd 调用 io_uring_register(2)。自内核 6.3 起可用。
-	FeatRegRegRing
-	FeatRecvSendBundle
+	_FeatRegRegRing
+	_FeatRecvSendBundle
 	// FeatMinTimeout
 	// 如果设置了该标志，则 io_uring 支持传递最小批处理等待超时。详情请参见 io_uring_submit_and_wait_min_timeout(3) 。
-	FeatMinTimeout
+	_FeatMinTimeout
 )
 
 const (
@@ -1306,7 +1341,6 @@ const (
 	EnterSQWakeup
 	EnterSQWait
 	EnterExtArg
-	EnterRegisteredRing
 )
 
 const (
