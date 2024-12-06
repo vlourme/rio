caa0ed530463cf87cca73edfcd7dc45620f5caa3
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-06 21:12:44 +0800

    add round pow 2

---
+++ pkg/aio/round2_32.go
@@ -0,0 +1,24 @@
+//go:build !amd64 && !arm64 && !ppc64 && !ppc64le && !riscv64 && !s390x
+
+package aio
+
+import "math"
+
+func RoundupPow2(n int) int {
+	if n < 1 {
+		return 0
+	}
+
+	x := uint64(n - 1)
+	x |= x >> 1
+	x |= x >> 2
+	x |= x >> 4
+	x |= x >> 8
+	x |= x >> 16
+
+	if x >= uint32(math.MaxInt32) {
+		return math.MaxInt32
+	}
+
+	return int(x + 1)
+}
