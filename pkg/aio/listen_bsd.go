54dbe05f07824ae7d863e73a2a3c3d03ab64cfaf
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-09 15:07:57 +0800

    add accept

--- pkg/aio/listen_bsd.go
+++ pkg/aio/listen_bsd.go
@@ -3,14 +3,49 @@
 package aio
 
 import (
-	"net"
+	"os"
+	"syscall"
 )
 
-func newListenerFd(network string, family int, sotype int, proto int, addr net.Addr, multicastInterface *net.Interface) (v *netFd, err error) {
-
-	return
-}
-
 func Accept(fd NetFd, cb OperationCallback) {
+	sock, sa, accpetErr := syscall.Accept(fd.Fd())
+	if accpetErr != nil {
+		cb(0, Userdata{}, os.NewSyscallError("accept", accpetErr))
+		return
+	}
+	// get local addr
+	lsa, lsaErr := syscall.Getsockname(sock)
+	if lsaErr != nil {
+		_ = syscall.Close(sock)
+		cb(0, Userdata{}, os.NewSyscallError("getsockname", lsaErr))
+		return
+	}
+	la := SockaddrToAddr(fd.Network(), lsa)
 
+	ra := SockaddrToAddr(fd.Network(), sa)
+
+	op := ReadOperator(fd)
+	userdata := op.userdata
+	// conn
+	conn := &netFd{
+		handle:     sock,
+		network:    fd.Network(),
+		family:     fd.Family(),
+		socketType: fd.SocketType(),
+		protocol:   fd.Protocol(),
+		localAddr:  la,
+		remoteAddr: ra,
+		rop:        Operator{},
+		wop:        Operator{},
+	}
+	conn.rop.fd = conn
+	conn.wop.fd = conn
+
+	userdata.Fd = conn
+	// cb
+	cb(sock, userdata, nil)
 }
+
+var (
+	somaxconn = syscall.SOMAXCONN
+)
