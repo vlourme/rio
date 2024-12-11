ef73d87a257b4bef6d7ef20a6e51b4ea7fdef938
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-11 19:31:33 +0800

    mod

--- pkg/aio/addr.go
+++ pkg/aio/addr.go
@@ -339,3 +339,59 @@ func RawToSockaddr(rsa *syscall.RawSockaddrAny) (syscall.Sockaddr, error) {
 	}
 	return nil, errors.New("aio.AddrToSockaddr: sockaddr type is invalid")
 }
+
+func tcpAddrToSockaddr(family int, addr *net.TCPAddr) (syscall.Sockaddr, error) {
+	if addr == nil {
+		return nil, nil
+	}
+	return ipToSockaddr(family, addr.IP, addr.Port, addr.Zone)
+}
+
+func ipToSockaddr(family int, ip net.IP, port int, zone string) (syscall.Sockaddr, error) {
+	switch family {
+	case syscall.AF_INET:
+		sa, err := ipToSockaddrInet4(ip, port)
+		if err != nil {
+			return nil, err
+		}
+		return &sa, nil
+	case syscall.AF_INET6:
+		sa, err := ipToSockaddrInet6(ip, port, zone)
+		if err != nil {
+			return nil, err
+		}
+		return &sa, nil
+	}
+	return nil, &net.AddrError{Err: "invalid address family", Addr: ip.String()}
+}
+
+func ipToSockaddrInet4(ip net.IP, port int) (syscall.SockaddrInet4, error) {
+	if len(ip) == 0 {
+		ip = net.IPv4zero
+	}
+	ip4 := ip.To4()
+	if ip4 == nil {
+		return syscall.SockaddrInet4{}, &net.AddrError{Err: "non-IPv4 address", Addr: ip.String()}
+	}
+	sa := syscall.SockaddrInet4{Port: port}
+	copy(sa.Addr[:], ip4)
+	return sa, nil
+}
+
+func ipToSockaddrInet6(ip net.IP, port int, zone string) (syscall.SockaddrInet6, error) {
+	if len(ip) == 0 || ip.Equal(net.IPv4zero) {
+		ip = net.IPv6zero
+	}
+	ip6 := ip.To16()
+	if ip6 == nil {
+		return syscall.SockaddrInet6{}, &net.AddrError{Err: "non-IPv6 address", Addr: ip.String()}
+	}
+	zoneId := uint32(0)
+	ifi, _ := net.InterfaceByName(zone)
+	if ifi != nil {
+		zoneId = uint32(ifi.Index)
+	}
+	sa := syscall.SockaddrInet6{Port: port, ZoneId: zoneId}
+	copy(sa.Addr[:], ip6)
+	return sa, nil
+}
