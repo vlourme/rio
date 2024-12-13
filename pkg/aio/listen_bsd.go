8ed81f59bec3a33afd770e35a6e9304064c6bc8b
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-13 16:48:55 +0800

    mod

--- pkg/aio/listen_bsd.go
+++ pkg/aio/listen_bsd.go
@@ -3,91 +3,9 @@
 package aio
 
 import (
-	"errors"
-	"os"
 	"runtime"
 	"syscall"
 )
-
-func Accept(fd NetFd, cb OperationCallback) {
-	op := ReadOperator(fd)
-	op.userdata.Fd = fd
-	op.callback = cb
-	op.completion = func(result int, cop *Operator, err error) {
-		completeAccept(result, cop, err)
-		runtime.KeepAlive(op)
-	}
-	cylinder := nextKqueueCylinder()
-	if err := cylinder.prepareRead(fd.Fd(), op); err != nil {
-		cb(0, Userdata{}, err)
-		// reset
-		op.callback = nil
-		op.completion = nil
-	}
-	return
-}
-
-func completeAccept(result int, cop *Operator, err error) {
-	userdata := Userdata{}
-	cb := cop.callback
-	if err != nil {
-		cb(0, userdata, err)
-		return
-	}
-
-	ln := cop.userdata.Fd.(NetFd)
-	if result == 0 {
-		Accept(ln, cb)
-		return
-	}
-	//TODO: 可能会堵住。测试堵是否会影响外部的投递。
-	sock, sa, acceptErr := syscall.Accept(ln.Fd())
-	runtime.KeepAlive(ln)
-	if acceptErr != nil {
-		if errors.Is(acceptErr, syscall.EAGAIN) {
-			Accept(ln, cb)
-			return
-		}
-		cb(0, userdata, os.NewSyscallError("accept", acceptErr))
-		return
-	}
-	syscall.CloseOnExec(sock)
-	if setNonblockErr := syscall.SetNonblock(sock, true); setNonblockErr != nil {
-		cb(0, userdata, os.NewSyscallError("setnonblock", setNonblockErr))
-		return
-	}
-	// get local addr
-	lsa, lsaErr := syscall.Getsockname(sock)
-	if lsaErr != nil {
-		_ = syscall.Close(sock)
-		cb(0, Userdata{}, os.NewSyscallError("getsockname", lsaErr))
-		return
-	}
-	la := SockaddrToAddr(ln.Network(), lsa)
-
-	ra := SockaddrToAddr(ln.Network(), sa)
-
-	// conn
-	conn := &netFd{
-		handle:     sock,
-		network:    ln.Network(),
-		family:     ln.Family(),
-		socketType: ln.SocketType(),
-		protocol:   ln.Protocol(),
-		ipv6only:   ln.IPv6Only(),
-		localAddr:  la,
-		remoteAddr: ra,
-		rop:        Operator{},
-		wop:        Operator{},
-	}
-	conn.rop.fd = conn
-	conn.wop.fd = conn
-
-	userdata.Fd = conn
-
-	cb(sock, userdata, nil)
-	return
-}
 
 var (
 	somaxconn = maxListenerBacklog()
@@ -99,10 +17,13 @@ func maxListenerBacklog() int {
 		err error
 	)
 	switch runtime.GOOS {
-	case "darwin":
+	case "darwin", "ios":
 		n, err = syscall.SysctlUint32("kern.ipc.somaxconn")
 	case "freebsd":
 		n, err = syscall.SysctlUint32("kern.ipc.soacceptqueue")
+	case "netbsd":
+	case "openbsd":
+		n, err = syscall.SysctlUint32("kern.somaxconn")
 	}
 	if n == 0 || err != nil {
 		return syscall.SOMAXCONN
--- pkg/aio/listen_linux.go
+++ pkg/aio/listen_linux.go
@@ -5,104 +5,11 @@ package aio
 import (
 	"bufio"
 	"os"
-	"runtime"
-	"strconv"
-	"strings"
 	"syscall"
-	"unsafe"
 )
-
-func Accept(fd NetFd, cb OperationCallback) {
-	// op
-	op := ReadOperator(fd)
-	// ln
-	lnFd := fd.Fd()
-	// msg
-	rsa, rsaLen := op.userdata.Msg.BuildRawSockaddrAny()
-	addrPtr := uintptr(unsafe.Pointer(rsa))
-	addrLenPtr := uint64(uintptr(unsafe.Pointer(&rsaLen)))
-
-	// cb
-	op.callback = cb
-	// completion
-	op.completion = func(result int, cop *Operator, err error) {
-		completeAccept(result, cop, err)
-		runtime.KeepAlive(op)
-	}
-
-	// prepare
-	err := prepare(opAccept, lnFd, addrPtr, 0, addrLenPtr, 0, op)
-	runtime.KeepAlive(op)
-	if err != nil {
-		cb(0, op.userdata, os.NewSyscallError("io_uring_prep_accept", err))
-		// reset
-		op.callback = nil
-		op.completion = nil
-		return
-	}
-	return
-}
-
-func completeAccept(result int, op *Operator, err error) {
-	// cb
-	cb := op.callback
-	// userdata
-	userdata := op.userdata
-	if err != nil {
-		cb(result, userdata, os.NewSyscallError("io_uring_prep_accept", err))
-		return
-	}
-	// conn
-	connFd := result
-	// ln
-	ln, _ := op.fd.(NetFd)
-	// addr
-	// get local addr
-	lsa, lsaErr := syscall.Getsockname(connFd)
-	if lsaErr != nil {
-		_ = syscall.Close(connFd)
-		op.callback(result, userdata, os.NewSyscallError("getsockname", lsaErr))
-		return
-	}
-	la := SockaddrToAddr(ln.Network(), lsa)
-
-	// get remote addr
-	rsa, rsaErr := syscall.Getpeername(connFd)
-	if rsaErr != nil {
-		_ = syscall.Close(connFd)
-		op.callback(result, userdata, os.NewSyscallError("getpeername", rsaErr))
-		return
-	}
-	ra := SockaddrToAddr(ln.Network(), rsa)
-
-	// conn
-	conn := &netFd{
-		handle:     connFd,
-		network:    ln.Network(),
-		family:     ln.Family(),
-		socketType: ln.SocketType(),
-		protocol:   ln.Protocol(),
-		ipv6only:   ln.IPv6Only(),
-		localAddr:  la,
-		remoteAddr: ra,
-		rop:        Operator{},
-		wop:        Operator{},
-	}
-	conn.rop.fd = conn
-	conn.wop.fd = conn
-
-	userdata.Fd = conn
-	// cb
-	cb(connFd, userdata, nil)
-	return
-}
 
 var (
 	somaxconn = maxListenerBacklog()
-)
-
-const (
-	maxUint16Value = 1<<16 - 1
 )
 
 func maxListenerBacklog() int {
@@ -111,27 +18,34 @@ func maxListenerBacklog() int {
 		return syscall.SOMAXCONN
 	}
 	defer fd.Close()
-
 	rd := bufio.NewReader(fd)
 
-	line, err := rd.ReadString('\n')
-	if err != nil {
+	l, readLineErr := rd.ReadString('\n')
+	if readLineErr != nil {
+		return syscall.SOMAXCONN
+	}
+	f := getFields(l)
+	n, _, ok := dtoi(f[0])
+	if n == 0 || !ok {
 		return syscall.SOMAXCONN
 	}
 
-	f := strings.Fields(line)
-	if len(f) < 1 {
-		return syscall.SOMAXCONN
+	if n > 1<<16-1 {
+		return maxAckBacklog(n)
+	}
+	return n
+}
+
+func maxAckBacklog(n int) int {
+	major, minor := KernelVersion()
+	size := 16
+	if major > 4 || (major == 4 && minor >= 1) {
+		size = 32
 	}
 
-	value, err := strconv.Atoi(f[0])
-	if err != nil || value == 0 {
-		return syscall.SOMAXCONN
+	var maxAck uint = 1<<size - 1
+	if uint(n) > maxAck {
+		n = int(maxAck)
 	}
-
-	if value > maxUint16Value {
-		value = maxUint16Value
-	}
-
-	return value
+	return n
 }
