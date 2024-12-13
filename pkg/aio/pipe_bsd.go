3c96d418ca721aa614f0a2bbb3ec0f401d500f94
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-13 16:48:11 +0800

    mod pipe2

---
+++ pkg/aio/pipe_bsd.go
@@ -0,0 +1,15 @@
+//go:build dragonfly || freebsd || netbsd || openbsd
+
+package aio
+
+import (
+	"os"
+	"syscall"
+)
+
+func Pipe2(p []int) error {
+	if err := syscall.Pipe2(p[:], syscall.O_NONBLOCK|syscall.O_CLOEXEC); err != nil {
+		return os.NewSyscallError("pipe2", err)
+	}
+	return nil
+}
