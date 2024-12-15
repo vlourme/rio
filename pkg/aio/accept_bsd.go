3065257d88cb9839e64b19e6e0e90cd51f4ac140
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-15 16:47:12 +0800

    mod accept

--- pkg/aio/accept_bsd.go
+++ pkg/aio/accept_bsd.go
@@ -10,16 +10,62 @@ import (
 )
 
 func Accept(fd NetFd, cb OperationCallback) {
-	sock, sa, acceptErr := syscall.Accept4(fd.Fd(), syscall.SOCK_NONBLOCK|syscall.SOCK_CLOEXEC)
-	runtime.KeepAlive(fd)
-	if acceptErr != nil {
-		if errors.Is(acceptErr, syscall.EAGAIN) || errors.Is(acceptErr, syscall.EINTR) || errors.Is(acceptErr, syscall.ECONNABORTED) {
-			Accept(fd, cb)
-			return
+	op := ReadOperator(fd)
+	op.userdata.Fd = fd
+	op.callback = cb
+	op.completion = func(result int, cop *Operator, err error) {
+		completeAccept(result, cop, err)
+		runtime.KeepAlive(op)
+	}
+
+	if timeout := op.timeout; timeout > 0 {
+		timer := getOperatorTimer()
+		op.timer = timer
+		timer.Start(timeout, &operatorCanceler{
+			op: op,
+		})
+	}
+
+	cylinder := nextKqueueCylinder()
+	if err := cylinder.prepareRead(fd.Fd(), op); err != nil {
+		cb(0, Userdata{}, err)
+		// reset
+		op.callback = nil
+		op.completion = nil
+		if timer := op.timer; timer != nil {
+			timer.Done()
+			putOperatorTimer(timer)
 		}
-		cb(0, Userdata{}, os.NewSyscallError("accept", acceptErr))
+	}
+}
+
+func completeAccept(result int, op *Operator, err error) {
+	cb := op.callback
+	userdata := op.userdata
+	if err != nil || result == 0 {
+		cb(0, userdata, err)
 		return
 	}
+	ln := userdata.Fd.(NetFd)
+	timer := op.timer
+	sock := 0
+	var sa syscall.Sockaddr
+	for {
+		if timer != nil && timer.DeadlineExceeded() {
+			cb(0, Userdata{}, ErrOperationDeadlineExceeded)
+			return
+		}
+		sock, sa, err = syscall.Accept4(ln.Fd(), syscall.SOCK_NONBLOCK|syscall.SOCK_CLOEXEC)
+		if err != nil {
+			if errors.Is(err, syscall.EAGAIN) || errors.Is(err, syscall.EINTR) || errors.Is(err, syscall.ECONNABORTED) {
+				continue
+			}
+			cb(0, Userdata{}, os.NewSyscallError("accept4", err))
+			return
+		}
+		break
+	}
+
 	// get local addr
 	lsa, lsaErr := syscall.Getsockname(sock)
 	if lsaErr != nil {
@@ -27,18 +73,18 @@ func Accept(fd NetFd, cb OperationCallback) {
 		cb(0, Userdata{}, os.NewSyscallError("getsockname", lsaErr))
 		return
 	}
-	la := SockaddrToAddr(fd.Network(), lsa)
+	la := SockaddrToAddr(ln.Network(), lsa)
 	// get remote addr
-	ra := SockaddrToAddr(fd.Network(), sa)
+	ra := SockaddrToAddr(ln.Network(), sa)
 
 	// conn
 	conn := &netFd{
 		handle:     sock,
-		network:    fd.Network(),
-		family:     fd.Family(),
-		socketType: fd.SocketType(),
-		protocol:   fd.Protocol(),
-		ipv6only:   fd.IPv6Only(),
+		network:    ln.Network(),
+		family:     ln.Family(),
+		socketType: ln.SocketType(),
+		protocol:   ln.Protocol(),
+		ipv6only:   ln.IPv6Only(),
 		localAddr:  la,
 		remoteAddr: ra,
 		rop:        Operator{},
@@ -47,10 +93,7 @@ func Accept(fd NetFd, cb OperationCallback) {
 	conn.rop.fd = conn
 	conn.wop.fd = conn
 
-	userdata := Userdata{
-		Fd: conn,
-	}
-
-	cb(sock, userdata, nil)
+	cb(sock, Userdata{Fd: conn}, nil)
+	runtime.KeepAlive(ln)
 	return
 }
