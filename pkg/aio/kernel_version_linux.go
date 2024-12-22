37fb58df05a18420849732402208a975d01d8fc5
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-22 14:49:37 +0800

    use once

--- pkg/aio/kernel_version_linux.go
+++ pkg/aio/kernel_version_linux.go
@@ -2,29 +2,41 @@
 
 package aio
 
-import "golang.org/x/sys/unix"
+import (
+	"golang.org/x/sys/unix"
+	"sync"
+)
+
+var (
+	kernelVersionMajor = 0
+	kernelVersionMinor = 0
+	kernelVersionOnce  = sync.Once{}
+)
 
 func KernelVersion() (major, minor int) {
-	var uname unix.Utsname
-	if err := unix.Uname(&uname); err != nil {
-		return
-	}
-	var (
-		values    [2]int
-		value, vi int
-	)
-	for _, c := range uname.Release {
-		if '0' <= c && c <= '9' {
-			value = (value * 10) + int(c-'0')
-		} else {
-			values[vi] = value
-			vi++
-			if vi >= len(values) {
-				break
-			}
-			value = 0
+	kernelVersionOnce.Do(func() {
+		var uname unix.Utsname
+		if err := unix.Uname(&uname); err != nil {
+			return
 		}
-	}
-
-	return values[0], values[1]
+		var (
+			values    [2]int
+			value, vi int
+		)
+		for _, c := range uname.Release {
+			if '0' <= c && c <= '9' {
+				value = (value * 10) + int(c-'0')
+			} else {
+				values[vi] = value
+				vi++
+				if vi >= len(values) {
+					break
+				}
+				value = 0
+			}
+		}
+		kernelVersionMajor = values[0]
+		kernelVersionMinor = values[1]
+	})
+	return kernelVersionMajor, kernelVersionMinor
 }
