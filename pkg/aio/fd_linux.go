e8d33cc2ec51546e0033a5e5c1ade6bc85d74abd
wangminxiang <ryougi.nevermore@hotmail.com>
2025-02-03 20:02:08 +0800

    mod close

--- pkg/aio/fd_linux.go
+++ pkg/aio/fd_linux.go
@@ -6,23 +6,41 @@ import (
 	"errors"
 	"runtime"
 	"syscall"
+	"time"
 )
 
 func Close(fd Fd, cb OperationCallback) {
 	op := fd.WriteOperator()
-	op.callback = cb
-	op.completion = completeClose
-	cylinder := nextIOURingCylinder()
-	op.setCylinder(cylinder)
-	err := cylinder.prepareRW(opClose, fd.Fd(), 0, 0, 0, 0, op.ptr())
-	if err != nil {
-		cb(Userdata{}, err)
-		op.reset()
+	ok := false
+	for i := 0; i < 10; i++ {
+		if op.hijacked.CompareAndSwap(false, true) {
+			op.callback = cb
+			op.completion = completeClose
+			cylinder := nextIOURingCylinder()
+			op.setCylinder(cylinder)
+			err := cylinder.prepareRW(opClose, fd.Fd(), 0, 0, 0, 0, op.ptr())
+			if err != nil {
+				cb(Userdata{}, err)
+				op.hijacked.Store(false)
+				op.reset()
+				break
+			}
+			ok = true
+			break
+		}
+		const (
+			waits = time.Nanosecond * 500
+		)
+		time.Sleep(waits)
+	}
+	if !ok {
+		CloseImmediately(fd)
 	}
 	runtime.KeepAlive(op)
 }
 
 func completeClose(_ int, op *Operator, err error) {
+	op.hijacked.Store(false)
 	if err != nil {
 		err = errors.Join(errors.New("aio.Operator: close failed"), err)
 		op.callback(Userdata{}, err)
