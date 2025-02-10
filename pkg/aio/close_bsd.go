a3d840f4fab95d4649eb10ca6f3656543617a30e
wangminxiang <ryougi.nevermore@hotmail.com>
2025-02-10 20:05:17 +0800

    mod close

--- pkg/aio/close_bsd.go
+++ pkg/aio/close_bsd.go
@@ -4,39 +4,19 @@ package aio
 
 import (
 	"github.com/brickingsoft/errors"
-	"os"
 	"syscall"
 )
 
-func Close(fd Fd, cb OperationCallback) {
-	switch fd.(type) {
-	case NetFd:
-		handle := fd.Fd()
-		err := syscall.Close(handle)
-		if err != nil {
-			err = errors.New(
-				"close failed",
-				errors.WithMeta(errMetaPkgKey, errMetaPkgVal),
-				errors.WithMeta(errMetaOpKey, errMetaOpClose),
-				errors.WithWrap(os.NewSyscallError("close", err)),
-			)
-			cb(Userdata{}, err)
-			return
-		}
-		cb(Userdata{}, nil)
-		return
-	default:
-		err := errors.New(
+func Close(fd Fd) (err error) {
+	sock := fd.Fd()
+	if err = syscall.Close(sock); err != nil {
+		err = errors.New(
 			"close failed",
 			errors.WithMeta(errMetaPkgKey, errMetaPkgVal),
 			errors.WithMeta(errMetaOpKey, errMetaOpClose),
-			errors.WithWrap(errors.Define("invalid fd")),
+			errors.WithWrap(err),
 		)
-		cb(Userdata{}, err)
 		return
 	}
-}
-
-func CloseImmediately(fd Fd) {
-	_ = syscall.Close(fd.Fd())
+	return
 }
--- pkg/aio/close_linux.go
+++ pkg/aio/close_linux.go
@@ -4,47 +4,19 @@ package aio
 
 import (
 	"github.com/brickingsoft/errors"
-	"os"
 	"syscall"
 )
 
-func Close(fd Fd, cb OperationCallback) {
-	op := acquireOperator(fd)
-	op.callback = cb
-	op.completion = completeClose
-	cylinder := nextIOURingCylinder()
-	op.setCylinder(cylinder)
-	err := cylinder.prepareRW(opClose, fd.Fd(), 0, 0, 0, 0, op.ptr())
-	if err != nil {
-		CloseImmediately(fd)
-		releaseOperator(op)
-		err = errors.New(
-			"close failed",
-			errors.WithMeta(errMetaPkgKey, errMetaPkgVal),
-			errors.WithMeta(errMetaOpKey, errMetaOpClose),
-			errors.WithWrap(os.NewSyscallError("io_uring_prep_close", err)),
-		)
-		cb(Userdata{}, err)
-	}
-}
-
-func completeClose(_ int, op *Operator, err error) {
-	cb := op.callback
-	releaseOperator(op)
-	if err != nil {
+func Close(fd Fd) (err error) {
+	sock := fd.Fd()
+	if err = syscall.Close(sock); err != nil {
 		err = errors.New(
 			"close failed",
 			errors.WithMeta(errMetaPkgKey, errMetaPkgVal),
 			errors.WithMeta(errMetaOpKey, errMetaOpClose),
 			errors.WithWrap(err),
 		)
-		cb(Userdata{}, err)
 		return
 	}
-	cb(Userdata{}, nil)
 	return
-}
-
-func CloseImmediately(fd Fd) {
-	_ = syscall.Close(fd.Fd())
 }
