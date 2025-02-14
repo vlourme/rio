eae86098d58272ea0a316e74bb17be93b3d222ca
wangminxiang <ryougi.nevermore@hotmail.com>
2025-02-14 21:44:57 +0800

    mod

--- pkg/ring/ring.go
+++ pkg/ring/ring.go
@@ -2,7 +2,6 @@ package ring
 
 import (
 	"context"
-	"fmt"
 	"github.com/brickingsoft/errors"
 	"github.com/brickingsoft/rio/pkg/sys"
 	"github.com/pawelgaczynski/giouring"
@@ -31,7 +30,7 @@ func New(size int) (*Ring, error) {
 		sendMsgZCEnable = true
 	}
 	// wait timeout
-	waitTimeout := syscall.NsecToTimespec((50 * time.Millisecond).Nanoseconds()) // todo wait timeout
+	waitTimeout := 50 * time.Millisecond // todo wait timeout opt
 	return &Ring{
 		ring:        r,
 		queue:       queue,
@@ -43,20 +42,18 @@ func New(size int) (*Ring, error) {
 				}
 			},
 		},
-		operationRCH: make(chan *Operation, size),
-		stopCh:       nil,
-		wg:           sync.WaitGroup{},
+		stopCh: nil,
+		wg:     sync.WaitGroup{},
 	}, nil
 }
 
 type Ring struct {
-	ring         *giouring.Ring
-	queue        *OperationQueue
-	waitTimeout  syscall.Timespec
-	operations   sync.Pool
-	operationRCH chan *Operation
-	stopCh       chan struct{}
-	wg           sync.WaitGroup
+	ring        *giouring.Ring
+	queue       *OperationQueue
+	waitTimeout time.Duration
+	operations  sync.Pool
+	stopCh      chan struct{}
+	wg          sync.WaitGroup
 }
 
 func (ring *Ring) AcquireOperation() *Operation {
@@ -67,8 +64,6 @@ func (ring *Ring) AcquireOperation() *Operation {
 func (ring *Ring) ReleaseOperation(op *Operation) {
 	if op.reset() {
 		ring.operations.Put(op)
-	} else {
-		ring.operationRCH <- op
 	}
 }
 
@@ -81,295 +76,160 @@ func (ring *Ring) Push(op *Operation) error {
 
 func (ring *Ring) Start(ctx context.Context) {
 	ring.stopCh = make(chan struct{}, 1)
-	ring.releasingOp(ctx)
+	ring.listenSQ(ctx)
+	ring.listenCQ(ctx)
+}
+
+func (ring *Ring) listenSQ(ctx context.Context) {
 	ring.wg.Add(1)
 	go func(ctx context.Context, ring *Ring) {
-		defer ring.wg.Done()
-
-		prepared := make([]*Operation, ring.queue.capacity)
-		cqes := make([]*giouring.CompletionQueueEvent, ring.queue.capacity)
-		zeroPeeked := 0
+		stopCh := ring.stopCh
+		queue := ring.queue
+		ready := make([]*Operation, queue.capacity) // todo batch opt
+		peekNothingTimes := 0
 		stopped := false
 		for {
 			select {
 			case <-ctx.Done():
 				stopped = true
 				break
-			case <-ring.stopCh:
+			case <-stopCh:
 				stopped = true
 				break
 			default:
+				peeked := queue.PeekBatch(ready)
+				if peeked == 0 {
+					peekNothingTimes++
+					if peekNothingTimes > 10 {
+						peekNothingTimes = 0
+						runtime.Gosched()
+					} else {
+						time.Sleep(500 * time.Nanosecond)
+					}
+					break
+				}
+				prepared := int64(0)
+				for i := int64(0); i < peeked; i++ {
+					op := ready[i]
+					if op == nil {
+						break
+					}
+					ready[i] = nil
+					sqe := ring.prepare(op)
+					runtime.KeepAlive(op)
+					if sqe == nil {
+						break
+					}
+					prepared++
+				}
+				if prepared == 0 {
+					break
+				}
 				// submit
-				submitted := ring.submit(prepared)
-				ring.waitAndHandleCQEs(cqes)
-				if submitted {
-					continue
+				for {
+					_, submitErr := ring.ring.Submit()
+					if submitErr != nil {
+						if errors.Is(submitErr, syscall.EAGAIN) || errors.Is(submitErr, syscall.EINTR) || errors.Is(submitErr, syscall.ETIME) {
+							continue
+						}
+						break
+					}
+					// adv queue
+					ring.queue.Advance(prepared)
+					break
 				}
-
-				zeroPeeked++
-				if zeroPeeked > 10 {
-					runtime.Gosched()
-				}
-				time.Sleep(500 * time.Nanosecond)
-				//time.Sleep(1 * time.Second)
-
-				// peek
-				//peeked := ring.queue.PeekBatch(prepared)
-				//if peeked == 0 {
-				//	ring.waitAndHandleCQEs(cqes)
-				//
-				//	zeroPeeked++
-				//	if zeroPeeked > 10 {
-				//		runtime.Gosched()
-				//	}
-				//	time.Sleep(500 * time.Nanosecond)
-				//	//time.Sleep(1 * time.Second)
-				//	continue
-				//}
-				//// prepare
-				//set := int64(0)
-				//for i := int64(0); i < peeked; i++ {
-				//	op := prepared[i]
-				//	if op == nil {
-				//		break
-				//	}
-				//	prepared[i] = nil
-				//	sqe := ring.prepare(op)
-				//	runtime.KeepAlive(op)
-				//	if sqe == nil {
-				//		break
-				//	}
-				//	set++
-				//}
-				//if set == 0 {
-				//	continue
-				//}
-				//// submit
-				//for {
-				//	_, submitErr := ring.ring.Submit()
-				//	if submitErr != nil {
-				//		if errors.Is(submitErr, syscall.EAGAIN) || errors.Is(submitErr, syscall.EINTR) || errors.Is(submitErr, syscall.ETIME) {
-				//			continue
-				//		}
-				//		break
-				//	}
-				//	ring.queue.Advance(set)
-				//	break
-				//}
-				//// wait cqe
-				//ring.waitAndHandleCQEs(cqes)
-				//for {
-				//	_, waitErr := ring.ring.WaitCQEs(1, &waitTimeout, nil)
-				//	if waitErr != nil {
-				//		if errors.Is(waitErr, syscall.EAGAIN) {
-				//			continue
-				//		}
-				//		cqReady = false
-				//		break
-				//	}
-				//	cqReady = true
-				//	break
-				//}
-				//if !cqReady {
-				//	continue
-				//}
-				//// peek cqe
-				//completed := ring.ring.PeekBatchCQE(cqes)
-				//fmt.Println("completed", completed)
-				//if completed == 0 {
-				//	continue
-				//}
-				//for i := uint32(0); i < completed; i++ {
-				//	cqe := cqes[i]
-				//	if cqe.UserData == 0 {
-				//		continue
-				//	}
-				//
-				//	cop := (*Operation)(unsafe.Pointer(uintptr(cqe.UserData)))
-				//	if cop.done.CompareAndSwap(false, true) {
-				//		fmt.Println("done: flags", cqe.Flags&giouring.CQEFMore, cqe.Flags&giouring.CQEFNotif)
-				//		// sent result when op not done (when done means timeout or ctx canceled)
-				//		var res int
-				//		var err error
-				//		if cqe.Res < 0 {
-				//			err = syscall.Errno(-cqe.Res)
-				//			// release hijacked when err occur
-				//			cop.hijacked.Store(false)
-				//		} else {
-				//			res = int(cqe.Res)
-				//		}
-				//		cop.ch <- Result{
-				//			N:   res,
-				//			Err: err,
-				//		}
-				//	} else {
-				//		fmt.Println("done:", cop.hijacked.Load(), string(cop.hijackedBytes))
-				//		// handle done but hijacked
-				//		// 1. by timeout or ctx canceled, so should be hijacked
-				//		// 2. by send_zc or sendmsg_zc, so should be hijacked
-				//		if cop.hijacked.CompareAndSwap(true, false) {
-				//			cop.ch <- Result{}
-				//		}
-				//	}
-				//}
-				//if completed > 0 {
-				//	ring.ring.CQAdvance(completed)
-				//}
 			}
 			if stopped {
 				break
 			}
 		}
-		// send failed for remains
+		// evict remain
 		if remains := ring.queue.Len(); remains > 0 {
-			peeked := ring.queue.PeekBatch(prepared)
+			peeked := ring.queue.PeekBatch(ready)
 			for i := int64(0); i < peeked; i++ {
-				op := prepared[i]
-				prepared[i] = nil
+				op := ready[i]
+				ready[i] = nil
 				op.ch <- Result{
 					N:   0,
 					Err: errors.New("uncompleted via closed"), // todo make err
 				}
 			}
 		}
-		fmt.Println("closed all pending operations")
-		// handle cached
-		close(ring.operationRCH)
-		// queue exit
-		ring.ring.QueueExit()
+		// wg done
+		ring.wg.Done()
 	}(ctx, ring)
 }
 
-func (ring *Ring) submit(prepared []*Operation) bool {
-	peeked := ring.queue.PeekBatch(prepared)
-	if peeked == 0 {
-		return false
-	}
-	set := int64(0)
-	for i := int64(0); i < peeked; i++ {
-		op := prepared[i]
-		if op == nil {
-			break
-		}
-		prepared[i] = nil
-		sqe := ring.prepare(op)
-		runtime.KeepAlive(op)
-		if sqe == nil {
-			break
-		}
-		set++
-	}
-	if set == 0 {
-		return false
-	}
-	// submit
-	for {
-		_, submitErr := ring.ring.Submit()
-		if submitErr != nil {
-			if errors.Is(submitErr, syscall.EAGAIN) || errors.Is(submitErr, syscall.EINTR) || errors.Is(submitErr, syscall.ETIME) {
-				continue
-			}
-			break
-		}
-		// adv queue
-		ring.queue.Advance(set)
-		return true
-	}
-	return false
-}
-
-func (ring *Ring) waitAndHandleCQEs(cqes []*giouring.CompletionQueueEvent) {
-	// wait cqe
-	cqReady := false
-	for {
-		_, waitErr := ring.ring.WaitCQEs(1, &ring.waitTimeout, nil)
-		if waitErr != nil {
-			if errors.Is(waitErr, syscall.EAGAIN) {
-				continue
-			}
-			cqReady = false
-			break
-		}
-		cqReady = true
-		break
-	}
-	if !cqReady {
-		return
-	}
-
-	completed := ring.ring.PeekBatchCQE(cqes)
-	fmt.Println("completed", completed)
-	if completed == 0 {
-		return
-	}
-	for i := uint32(0); i < completed; i++ {
-		cqe := cqes[i]
-		if cqe.UserData == 0 {
-			continue
-		}
-
-		cop := (*Operation)(unsafe.Pointer(uintptr(cqe.UserData)))
-		if cop.done.CompareAndSwap(false, true) {
-			fmt.Println("done: flags", cqe.Flags&giouring.CQEFMore, cqe.Flags&giouring.CQEFNotif)
-			// sent result when op not done (when done means timeout or ctx canceled)
-			var res int
-			var err error
-			if cqe.Res < 0 {
-				err = syscall.Errno(-cqe.Res)
-				// release hijacked when err occur
-				cop.hijacked.Store(false)
-			} else {
-				res = int(cqe.Res)
-			}
-			cop.ch <- Result{
-				N:   res,
-				Err: err,
-			}
-		} else {
-			fmt.Println("done:", cop.hijacked.Load(), string(cop.hijackedBytes))
-			// handle done but hijacked
-			// 1. by timeout or ctx canceled, so should be hijacked
-			// 2. by send_zc or sendmsg_zc, so should be hijacked
-			if cop.hijacked.CompareAndSwap(true, false) {
-				cop.ch <- Result{}
-			}
-		}
-	}
-	if completed > 0 {
-		ring.ring.CQAdvance(completed)
-	}
-
-	return
-}
-
-func (ring *Ring) releasingOp(ctx context.Context) {
-	// todo batch
+func (ring *Ring) listenCQ(ctx context.Context) {
 	ring.wg.Add(1)
 	go func(ctx context.Context, ring *Ring) {
+		stopCh := ring.stopCh
+		waitTimeout := syscall.NsecToTimespec(ring.waitTimeout.Nanoseconds()) // todo wait timeout
+		cq := make([]*giouring.CompletionQueueEvent, ring.queue.capacity)
 		stopped := false
 		for {
 			select {
 			case <-ctx.Done():
 				stopped = true
 				break
-			case <-ring.stopCh:
+			case <-stopCh:
 				stopped = true
 				break
-			case op, ok := <-ring.operationRCH:
-				if !ok {
-					stopped = true
+			default:
+				// wait
+				if _, waitErr := ring.ring.WaitCQEs(1, &waitTimeout, nil); waitErr != nil {
 					break
 				}
-				//fmt.Println("releasing operation", op.hijacked.Load(), string(op.hijackedBytes))
-				<-op.ch
-				ring.ReleaseOperation(op)
+				// peek
+				completed := ring.ring.PeekBatchCQE(cq)
+				if completed == 0 {
+					break
+				}
+				// handle
+				for i := uint32(0); i < completed; i++ {
+					cqe := cq[i]
+					cq[i] = nil
+					if cqe.UserData == 0 {
+						continue
+					}
+					if cqe.Flags&giouring.CQEFNotif != 0 {
+						// used by send_zc or sendmsg_ze, so continue
+						continue
+					}
+					// op
+					cop := (*Operation)(unsafe.Pointer(uintptr(cqe.UserData)))
+					if cop.done.CompareAndSwap(false, true) { // not done
+						// sent result when op not done (when done means timeout or ctx canceled)
+						var res int
+						var err error
+						if cqe.Res < 0 {
+							err = syscall.Errno(-cqe.Res)
+							// release hijacked when err occur
+							cop.hijacked.Store(false)
+						} else {
+							res = int(cqe.Res)
+						}
+						cop.ch <- Result{
+							N:   res,
+							Err: err,
+						}
+					} else { // done
+						// handle done but hijacked
+						// 1. by timeout or ctx canceled, so should be hijacked
+						// 2. by send_zc or sendmsg_zc, so should be hijacked
+						if cop.hijacked.CompareAndSwap(true, false) {
+							cop.ch <- Result{}
+						}
+					}
+				}
+				ring.ring.CQAdvance(completed)
 				break
 			}
 			if stopped {
 				break
 			}
 		}
-		//fmt.Println("releasing operation")
+		// wg done
 		ring.wg.Done()
 	}(ctx, ring)
 }
@@ -378,6 +238,7 @@ func (ring *Ring) Stop() {
 	if ring.stopCh != nil {
 		close(ring.stopCh)
 		ring.wg.Wait()
+		ring.ring.QueueExit()
 		return
 	}
 	ring.ring.QueueExit()
