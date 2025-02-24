ea3f5135939c933c5b6f0c771afd8f89aaf6dd50
wangminxiang <ryougi.nevermore@hotmail.com>
2025-02-24 20:08:26 +0800

    mod

--- addr.go
+++ addr.go
@@ -5,6 +5,18 @@ import (
 	"net"
 )
 
+func ResolveTCPAddr(address string) (*net.TCPAddr, error) {
+	addr, _, _, err := sys.ResolveAddr("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+	tcpAddr, ok := addr.(*net.TCPAddr)
+	if !ok {
+		return nil, err
+	}
+	return tcpAddr, nil
+}
+
 func ResolveUDPAddr(address string) (*net.UDPAddr, error) {
 	addr, _, _, err := sys.ResolveAddr("udp", address)
 	if err != nil {
@@ -15,4 +27,28 @@ func ResolveUDPAddr(address string) (*net.UDPAddr, error) {
 		return nil, err
 	}
 	return udpAddr, nil
+}
+
+func ResolveUnixAddr(address string) (*net.UnixAddr, error) {
+	addr, _, _, err := sys.ResolveAddr("unix", address)
+	if err != nil {
+		return nil, err
+	}
+	unixAddr, ok := addr.(*net.UnixAddr)
+	if !ok {
+		return nil, err
+	}
+	return unixAddr, nil
+}
+
+func ResolveIPAddr(address string) (*net.IPAddr, error) {
+	addr, _, _, err := sys.ResolveAddr("ip", address)
+	if err != nil {
+		return nil, err
+	}
+	ipAddr, ok := addr.(*net.IPAddr)
+	if !ok {
+		return nil, err
+	}
+	return ipAddr, nil
 }
