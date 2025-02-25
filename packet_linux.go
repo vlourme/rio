7e53e766020b5c9b6cd0ef6252aaf7347fc9713b
wangminxiang <ryougi.nevermore@hotmail.com>
2025-02-25 20:03:14 +0800

    rename

--- packet_linux.go
+++ packet_linux.go
@@ -8,14 +8,14 @@ import (
 	"net"
 )
 
-func ListenPacket(network string, addr string) (ln net.PacketConn, err error) {
+func ListenPacket(network string, addr string) (c net.PacketConn, err error) {
 	config := ListenConfig{}
 	ctx := context.Background()
-	ln, err = config.ListenPacket(ctx, network, addr)
+	c, err = config.ListenPacket(ctx, network, addr)
 	return
 }
 
-func (lc *ListenConfig) ListenPacket(ctx context.Context, network, address string) (conn net.PacketConn, err error) {
+func (lc *ListenConfig) ListenPacket(ctx context.Context, network, address string) (c net.PacketConn, err error) {
 	addr, _, _, addrErr := sys.ResolveAddr(network, address)
 	if addrErr != nil {
 		err = &net.OpError{Op: "listen", Net: network, Source: nil, Addr: nil, Err: addrErr}
@@ -23,7 +23,7 @@ func (lc *ListenConfig) ListenPacket(ctx context.Context, network, address strin
 	}
 	switch a := addr.(type) {
 	case *net.UDPAddr:
-		conn, err = lc.ListenUDP(ctx, network, a)
+		c, err = lc.ListenUDP(ctx, network, a)
 		break
 	case *net.IPAddr:
 		// todo
