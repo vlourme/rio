f2f4a6faf30f79032e60259b2dd799774c7b7dac
wangminxiang <ryougi.nevermore@hotmail.com>
2025-03-03 20:21:24 +0800

    rm vortex options

--- vortex_posix.go
+++ vortex_posix.go
@@ -3,31 +3,12 @@
 package rio
 
 import (
-	"github.com/brickingsoft/rio/pkg/iouring/aio"
 	"github.com/brickingsoft/rio/pkg/process"
 )
 
 func UseProcessPriority(level process.PriorityLevel) {
 	_ = process.SetCurrentProcessPriority(level)
 }
-
-func UsePreformMode() {}
-
-func UseFlags(flags uint32) {}
-
-func UseFeatures(feats uint32) {}
-
-func UseEntries(entries int) {}
-
-func UsePrepareBatchSize(size uint32) {}
-
-func UseVortexNum(n int) {}
-
-func UseLoadBalancer(lb aio.LoadBalancer) {}
-
-func UseWaitTransmissionBuilder(builder aio.TransmissionBuilder) {}
-
-func UseCPUAffinity(use bool) {}
 
 func Pin() error {
 	return nil
