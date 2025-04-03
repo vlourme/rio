e7543c2240b11083e980352ad663ceaaf78b8da2
wangminxiang <ryougi.nevermore@hotmail.com>
2025-04-03 15:42:50 +0800

    mod

--- tcp_test.go
+++ tcp_test.go
@@ -41,8 +41,11 @@ func TestTCP(t *testing.T) {
 		return
 	}()
 
+	src := make([]byte, 64)
+	_, _ = rand.Read(src)
+
 	wg.Add(1)
-	go func(ln net.Listener, wg *sync.WaitGroup) {
+	go func(ln net.Listener, src []byte, wg *sync.WaitGroup) {
 		defer wg.Done()
 		for {
 			conn, err := ln.Accept()
@@ -51,27 +54,49 @@ func TestTCP(t *testing.T) {
 				return
 			}
 			wg.Add(1)
-			go func(conn net.Conn, wg *sync.WaitGroup) {
+			go func(conn net.Conn, src []byte, wg *sync.WaitGroup) {
 				defer wg.Done()
 				t.Log("srv:", conn.LocalAddr(), conn.RemoteAddr())
-				b := make([]byte, 4096)
+
+				b := make([]byte, len(src))
+
+				var rn int
 				for {
-					rn, rErr := conn.Read(b)
-					t.Log("srv read", rn, rErr)
+					rnn, rErr := conn.Read(b[rn:])
 					if rErr != nil {
-						break
+						if errors.Is(rErr, io.EOF) {
+							t.Log("srv read EOF")
+							return
+						}
+						t.Error("srv read failed", rnn, rErr)
+						return
 					}
-					wn, wErr := conn.Write(b[:rn])
-					t.Log("srv write", wn, wErr)
-					if wErr != nil {
+					rn += rnn
+					if rn == len(b) {
 						break
 					}
 				}
+				t.Log("srv read succeed", rn, bytes.Equal(src, b))
+
+				var wn int
+				for {
+					wnn, wErr := conn.Write(b[wn:])
+					if wErr != nil {
+						t.Error("srv write failed", wnn, wErr)
+						return
+					}
+					wn += wnn
+					if wn == len(b) {
+						break
+					}
+				}
+				t.Log("srv write succeed", wn)
+
 				_ = conn.Close()
-			}(conn, wg)
+			}(conn, src, wg)
 			return
 		}
-	}(ln, wg)
+	}(ln, src, wg)
 
 	dialer := rio.DefaultDialer
 	conn, connErr := dialer.Dial("tcp", "127.0.0.1:9000")
@@ -82,18 +107,38 @@ func TestTCP(t *testing.T) {
 	t.Log("cli:", conn.LocalAddr(), conn.RemoteAddr())
 	defer conn.Close()
 
-	wn, wErr := conn.Write([]byte("hello world"))
-	t.Log("cli write", wn, wErr)
-	if wErr != nil {
-		t.Error(wn, wErr)
-		return
+	var wn int
+	for {
+		wnn, wErr := conn.Write(src[wn:])
+		if wErr != nil {
+			t.Error("cli write failed", wn, wErr)
+			return
+		}
+		wn += wnn
+		if wn == len(src) {
+			break
+		}
 	}
-	b := make([]byte, 1024)
-	rn, rErr := conn.Read(b)
-	t.Log("cli read", rn, string(b[:rn]), rErr)
-	if rErr != nil {
-		t.Error(rErr)
-		return
+	t.Log("cli write succeed", wn)
+
+	var rn int
+	p := make([]byte, len(src))
+	for {
+		rnn, rErr := conn.Read(p[rn:])
+		if rErr != nil {
+			t.Error("cli read failed", rn, rErr)
+			return
+		}
+		rn += rnn
+		if rn == len(src) {
+			break
+		}
+	}
+	same := bytes.Equal(src, p)
+	if same {
+		t.Log("cli read succeed", rn, same)
+	} else {
+		t.Error("cli read failed", wn, rn)
 	}
 
 }
