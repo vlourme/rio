67c4bc63167bbddd5d4f75d01fc1cd579a82614a
wangminxiang <ryougi.nevermore@hotmail.com>
2025-04-20 17:00:17 +0800

    mod curve

--- pkg/liburing/aio/transmission.go
+++ pkg/liburing/aio/transmission.go
@@ -48,7 +48,7 @@ func NewCurveTransmission(curve Curve) Transmission {
 		timeout := syscall.NsecToTimespec(t.Timeout.Nanoseconds())
 		times = append(times, WaitNTime{
 			n:    n,
-			time: timeout,
+			time: &timeout,
 		})
 	}
 	return &CurveTransmission{
@@ -60,7 +60,7 @@ func NewCurveTransmission(curve Curve) Transmission {
 
 type WaitNTime struct {
 	n    uint32
-	time syscall.Timespec
+	time *syscall.Timespec
 }
 
 type CurveTransmission struct {
@@ -74,30 +74,30 @@ func (tran *CurveTransmission) Match(n uint32) (uint32, *syscall.Timespec) {
 	for i := 0; i < tran.size; i++ {
 		right := tran.curve[i]
 		if left.n <= n && n < right.n {
-			return right.n, &right.time
+			return right.n, right.time
 		}
 		left = right
 	}
 	tail := tran.curve[tran.size-1]
-	return tail.n, &tail.time
+	return tail.n, tail.time
 }
 
 func (tran *CurveTransmission) Up() (uint32, *syscall.Timespec) {
 	if tran.idx == tran.size-1 {
 		tail := tran.curve[tran.idx]
-		return tail.n, &tail.time
+		return tail.n, tail.time
 	}
 	tran.idx++
 	node := tran.curve[tran.idx]
-	return node.n, &node.time
+	return node.n, node.time
 }
 
 func (tran *CurveTransmission) Down() (uint32, *syscall.Timespec) {
 	if tran.idx < 1 {
 		head := tran.curve[0]
-		return head.n, &head.time
+		return head.n, head.time
 	}
 	tran.idx--
 	node := tran.curve[tran.idx]
-	return node.n, &node.time
+	return node.n, node.time
 }
